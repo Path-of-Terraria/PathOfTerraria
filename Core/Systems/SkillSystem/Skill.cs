2fa1dd07ca944f1b6982162d00d1447a0f203157
bobfishkins23 <bobfishkins23@yahoo.com>
2023-07-15 23:03:23 -0400

    Architecture

--- Core/Systems/SkillSystem/Skill.cs
+++ Core/Systems/SkillSystem/Skill.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FunnyExperience.Content.Items.Gear;
 
 namespace FunnyExperience.Core.Systems.SkillSystem
 {
-	internal class Skill
+	internal abstract class Skill
 	{
+		public int duration;
+		public int timer;
+
+		public int maxCooldown;
+		public int cooldown;
+
+		public int manaCost;
+
+		public GearType weaponType;
+
+		public virtual string Name => GetType().Name;
+		public virtual string Texture => "FunnyExperience/Assets/Skills/" + GetType().Name;
+
+		/// <summary>
+		/// What this skill actually does
+		/// </summary>
+		/// <param name="player">The player using the skill</param>
+		public abstract void UseSkill(Player player);
+
+		/// <summary>
+		/// Gets the description of this skill
+		/// </summary>
+		/// <param name="player">The player that has the skill</param>
+		/// <returns>The skill's description</returns>
+		public abstract string GetDescription(Player player);
+
+		/// <summary>
+		/// If this skill should be able to be used. By default this is if the cooldown is over and the player has enough mana.
+		/// </summary>
+		/// <param name="player">The player trying to use the skill</param>
+		/// <returns>If the skill can be used or not</returns>
+		public virtual bool CanUseSkill(Player player)
+		{
+			return cooldown <= 0 && player.CheckMana(manaCost);
+		}
 	}
 }
