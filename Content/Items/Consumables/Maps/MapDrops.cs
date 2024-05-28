60fcc57be6505ee7846240ccc5c1e32936f768f1
CollinHerber <rustaddicts@gmail.com>
2024-05-28 04:48:21 -0400

    feat(#1): add in map drop chance for kingslime and global

---
+++ Content/Items/Consumables/Maps/MapDrops.cs
@@ -0,0 +1,17 @@
+﻿using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+
+namespace PathOfTerraria.Content.Items.Consumables.Maps;
+
+internal class MapDrops : GlobalNPC
+{
+	public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot) {
+		if (npc.type == NPCID.KingSlime) {
+			npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<LowTierMap>()));
+		}
+		
+		if (NPC.downedSlimeKing) {
+			npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<LowTierMap>(), 300));
+		}
+	}
+}
