fe8dbdf29f28aacbad5648d40a056a04fb0ff9ea
Collin Herber <rustaddicts@gmail.com>
2024-06-11 21:01:12 -0400

    chore: remove comment from position

--- Data/Models/PassiveData.cs
+++ Data/Models/PassiveData.cs
@@ -6,7 +6,7 @@ public class PassiveData
 {
 	public string InternalIdentifier { get; set; }
 	public int ReferenceId { get; set; }
-	public PassivePosition Position { get; set; } // no clue how this works but it probably works fine like this - Position[0] -> x | Position[1] -> y
+	public PassivePosition Position { get; set; }
 	public int MaxLevel { get; set; }
 	public List<PassiveConnection> Connections { get; set; }
 }
