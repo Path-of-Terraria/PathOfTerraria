37209eef7a49d469dac766cada82ef1b12c5915c
Collin Herber <rustaddicts@gmail.com>
2024-06-18 16:28:31 -0400

    feat: add in hand positioning and projectile repositioning on hand

--- Content/Items/Gear/Weapons/Whip/AltWhipHook.cs
+++ Content/Items/Gear/Weapons/Whip/AltWhipHook.cs
@@ -29,9 +29,7 @@ internal class AltWhipHook : ILoadable
 				flip = true;
 			}
 
-			Player plr = Main.player[proj.owner];
-
-			Vector2 origin = plr.Center - new Vector2(0, 8);
+			Vector2 origin = CalculateHandPosition(proj);
 			Vector2 extendedPosition = GetExtendedPosition(proj, rangeMult, segments - 1, flip) * (1f - Math.Abs(factor - 0.5f) * 2);
 			Vector2 midPoint = origin + extendedPosition.RotatedBy(rotationFactor * 0.25f) / 2;
 			Vector2 target = origin + extendedPosition.RotatedBy(rotationFactor);
@@ -46,6 +44,26 @@ internal class AltWhipHook : ILoadable
 		{
 			orig(proj, controlPoints);
 		}
+	}
+
+	private Vector2 CalculateHandPosition(Projectile proj)
+	{
+		float rotationOffset = proj.frameCounter switch
+		{
+			>= 50 => MathHelper.Pi,
+			>= 40 => 3 * MathHelper.PiOver4,
+			>= 30 => MathHelper.PiOver2,
+			_ => -MathHelper.PiOver2
+		};
+		
+		Player owner = Main.player[proj.owner];
+		// Calculate the arm's position and direction
+		Vector2 armPosition = owner.Center;
+		const float armLength = 10f;
+
+		// Calculate the hand's position based on the arm's rotation
+		Vector2 handOffset = new Vector2((float)Math.Cos(rotationOffset), (float)Math.Sin(rotationOffset)) * armLength;
+		return armPosition + new Vector2(handOffset.X + 20, handOffset.Y + 28);
 	}
 
 	private static Vector2 GetExtendedPosition(Projectile proj, float rangeMult, int i, bool flip)
