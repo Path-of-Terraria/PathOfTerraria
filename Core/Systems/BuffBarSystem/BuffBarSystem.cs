2d9f857562be1ba7800a6133ccefcd9d49537134
GabeHasWon <gabriel@guevarameyer.com>
2024-06-26 18:23:49 -0500

    fixed buff icon clickbox locations

--- Core/Systems/BuffBarSystem/BuffBarSystem.cs
+++ Core/Systems/BuffBarSystem/BuffBarSystem.cs
@@ -11,6 +11,7 @@ public class BuffBarSystem : GlobalBuff
 	{
 		const int buffPositionOffsetY = 20;
 		drawParams.Position = new Vector2(drawParams.Position.X, drawParams.Position.Y + buffPositionOffsetY);
+		drawParams.MouseRectangle.Y += buffPositionOffsetY;
 		return true;
 	}
 }
