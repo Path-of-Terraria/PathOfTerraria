8a80dc88a153cb3bb7dea4457d467c592abde34c
GabeHasWon <gabriel@guevarameyer.com>
2024-07-17 12:02:32 -0500

    disable all phm vanilla boss spawning methods aside from evil orbs

---
+++ Core/Systems/VanillaModifications/CraftingDisabler.cs
@@ -0,0 +1,16 @@
+﻿namespace PathOfTerraria.Core.Systems.VanillaModifications;
+
+internal class CraftingDisabler : ModSystem
+{
+	public override void PostAddRecipes()
+	{
+		foreach (Recipe recipe in Main.recipe)
+		{
+			// Disable all relevant vanilla boss spawners
+			if (BossGlobalItemDisabler.BossSpawners.Contains(recipe.createItem.type))
+			{
+				recipe.DisableRecipe();
+			}
+		}
+	}
+}
