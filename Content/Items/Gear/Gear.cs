01ee41a4a03577a8a91ba53d8175f6466c93309e
GabeHasWon <gabriel@guevarameyer.com>
2024-07-19 12:11:33 -0500

    check that hovered stuff is actually gear

--- Content/Items/Gear/Gear.cs
+++ Content/Items/Gear/Gear.cs
@@ -74,24 +74,24 @@ internal abstract class Gear : PoTItem
 
 	public sealed override void SwapItemModifiers(EntityModifier SawpItemModifier)
 	{
-		if (Item.headSlot >= 0 && Main.LocalPlayer.armor[0].active)
+		if (Item.headSlot >= 0 && Main.LocalPlayer.armor[0].active && Main.LocalPlayer.armor[0].ModItem is Gear headGear)
 		{
-			(Main.LocalPlayer.armor[0].ModItem as Gear).ApplyAffixes(SawpItemModifier);
+			headGear.ApplyAffixes(SawpItemModifier);
 		}
-		else if (Item.bodySlot >= 0 && Main.LocalPlayer.armor[1].active)
+		else if (Item.bodySlot >= 0 && Main.LocalPlayer.armor[1].active && Main.LocalPlayer.armor[0].ModItem is Gear bodyGear)
 		{
-			(Main.LocalPlayer.armor[1].ModItem as Gear).ApplyAffixes(SawpItemModifier);
+			bodyGear.ApplyAffixes(SawpItemModifier);
 		}
-		else if (Item.legSlot >= 0 && Main.LocalPlayer.armor[2].active)
+		else if (Item.legSlot >= 0 && Main.LocalPlayer.armor[2].active && Main.LocalPlayer.armor[0].ModItem is Gear legsGear)
 		{
-			(Main.LocalPlayer.armor[2].ModItem as Gear).ApplyAffixes(SawpItemModifier);
+			legsGear.ApplyAffixes(SawpItemModifier);
 		}
 		// missing accessories
 		else if (Item.damage > 0)
 		{
-			if (Main.LocalPlayer.inventory[0].ModItem is Gear)
+			if (Main.LocalPlayer.inventory[0].ModItem is Gear gear)
 			{
-				(Main.LocalPlayer.inventory[0].ModItem as Gear).ApplyAffixes(SawpItemModifier);
+				gear.ApplyAffixes(SawpItemModifier);
 			}
 		}
 	}
