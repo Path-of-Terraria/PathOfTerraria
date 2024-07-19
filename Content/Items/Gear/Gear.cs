ad4de9ac85975de878da51c62097c96f0d4ddaae
GabeHasWon <gabriel@guevarameyer.com>
2024-07-19 12:12:23 -0500

    rename param

--- Content/Items/Gear/Gear.cs
+++ Content/Items/Gear/Gear.cs
@@ -72,26 +72,26 @@ internal abstract class Gear : PoTItem
 		_sockets.Where(s => s is not null).ToList().ForEach(s => s.UpdateEquip(player, Item));
 	}
 
-	public sealed override void SwapItemModifiers(EntityModifier SawpItemModifier)
+	public sealed override void SwapItemModifiers(EntityModifier swapItemModifier)
 	{
 		if (Item.headSlot >= 0 && Main.LocalPlayer.armor[0].active && Main.LocalPlayer.armor[0].ModItem is Gear headGear)
 		{
-			headGear.ApplyAffixes(SawpItemModifier);
+			headGear.ApplyAffixes(swapItemModifier);
 		}
 		else if (Item.bodySlot >= 0 && Main.LocalPlayer.armor[1].active && Main.LocalPlayer.armor[0].ModItem is Gear bodyGear)
 		{
-			bodyGear.ApplyAffixes(SawpItemModifier);
+			bodyGear.ApplyAffixes(swapItemModifier);
 		}
 		else if (Item.legSlot >= 0 && Main.LocalPlayer.armor[2].active && Main.LocalPlayer.armor[0].ModItem is Gear legsGear)
 		{
-			legsGear.ApplyAffixes(SawpItemModifier);
+			legsGear.ApplyAffixes(swapItemModifier);
 		}
 		// missing accessories
 		else if (Item.damage > 0)
 		{
 			if (Main.LocalPlayer.inventory[0].ModItem is Gear gear)
 			{
-				gear.ApplyAffixes(SawpItemModifier);
+				gear.ApplyAffixes(swapItemModifier);
 			}
 		}
 	}
