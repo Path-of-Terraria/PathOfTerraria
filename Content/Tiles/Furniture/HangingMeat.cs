c07df9c1cf76f90305afb7d5e7f535cc1eeb3410
shnakamura <joaovtnakmura@gmail.com>
2024-07-22 13:13:27 -0300

    Implements Deer Trophy & Hanging Meat

---
+++ Content/Tiles/Furniture/HangingMeat.cs
@@ -0,0 +1,44 @@
+using System.Reflection;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent.Drawing;
+using Terraria.ID;
+using Terraria.ObjectData;
+
+namespace PathOfTerraria.Content.Tiles.Furniture;
+
+public class HangingMeat : ModTile
+{
+	public override string Texture { get; } = $"{nameof(PathOfTerraria)}/Assets/Tiles/Furniture/{nameof(HangingMeat)}";
+
+	public override void SetStaticDefaults()
+	{
+		Main.tileFrameImportant[Type] = true;
+		Main.tileBlockLight[Type] = true;
+		Main.tileLavaDeath[Type] = true;
+		Main.tileNoAttach[Type] = true;
+		
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+
+		TileObjectData.newTile.Height = 3;
+		TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16 };
+
+		TileObjectData.newTile.StyleHorizontal = true;
+		TileObjectData.newTile.RandomStyleRange = 2;
+
+		TileObjectData.newTile.Origin = Point16.Zero;
+		
+		TileObjectData.newTile.AnchorTop = new AnchorData(AnchorType.SolidTile, 1, 0);
+		TileObjectData.newTile.AnchorBottom = AnchorData.Empty;
+		
+		TileObjectData.addTile(Type);
+
+		DustType = DustID.Blood;
+		HitSound = SoundID.Dig;
+	}
+
+	public override void NumDust(int i, int j, bool fail, ref int num)
+	{
+		num = fail ? 1 : 3;
+	}
+}
