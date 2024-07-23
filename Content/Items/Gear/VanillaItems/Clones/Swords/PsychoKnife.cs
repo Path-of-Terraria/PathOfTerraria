d77bedf64613b6d40f9c0fcff03ae359874d0bbb
GabeHasWon <gabriel@guevarameyer.com>
2024-07-23 10:00:57 -0500

    finished psycho knife

--- Content/Items/Gear/VanillaItems/Clones/Swords/PsychoKnife.cs
+++ Content/Items/Gear/VanillaItems/Clones/Swords/PsychoKnife.cs
@@ -1,4 +1,7 @@
-﻿using Terraria.ID;
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using Terraria.DataStructures;
+using Terraria.ID;
 
 namespace PathOfTerraria.Content.Items.Gear.VanillaItems.Clones.Swords;
 
@@ -6,65 +9,134 @@ internal class PsychoKnife : VanillaClone
 {
 	protected override short VanillaItemId => ItemID.PsychoKnife;
 
+	public override void Load()
+	{
+		IL_Player.Update += FixStealthBeingResetAnnoyingly;
+	}
+
+	private void FixStealthBeingResetAnnoyingly(ILContext il)
+	{
+		ILCursor c = new(il);
+
+		while (c.TryGotoNext(x => x.MatchStfld<Player>(nameof(Player.stealth))))
+		{ }
+
+		c.Emit(OpCodes.Pop);
+		c.Emit(OpCodes.Ldarg_0);
+		c.EmitDelegate(ResetStealthIfNeeded);
+
+		c.Index++;
+		c.Emit(OpCodes.Ldarg_0);
+	}
+	
+	public static float ResetStealthIfNeeded(Player player)
+	{
+		if (player.HeldItem.type == ModContent.ItemType<PsychoKnife>())
+		{
+			return player.stealth;
+		}
+
+		return 1f;
+	}
+
 	public override void Defaults()
 	{
 		ItemType = Core.ItemType.Melee;
 		base.Defaults();
 	}
 
-	public override void HoldItem(Player player)
+	public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
 	{
-		if (player.itemAnimation > 0)
-		{
-			player.stealthTimer = 15;
+		player.stealth = 1;
 
-			if (player.stealth > 0f)
-			{
-				player.stealth += 0.1f;
-			}
+		if (Main.netMode == NetmodeID.MultiplayerClient)
+		{
+			NetMessage.SendData(MessageID.PlayerStealth, -1, -1, null, player.whoAmI);
 		}
-		else if (player.velocity.Length() < 0.141421363f && !player.mount.Active)
-		{
-			if (player.stealthTimer == 0 && player.stealth > 0f)
-			{
-				player.stealth -= 0.02f;
-				if (player.stealth <= 0)
-				{
-					player.stealth = 0f;
+	}
 
-					if (Main.netMode == NetmodeID.MultiplayerClient)
+	public class PsychoKnifePlayer : ModPlayer
+	{
+		public override void DrawEffects(PlayerDrawSet drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright)
+		{
+			if (Player.HeldItem.type != ModContent.ItemType<PsychoKnife>())
+			{
+				return;
+			}
+
+			r *= Player.stealth;
+			g *= Player.stealth;
+			b *= Player.stealth;
+			a *= Player.stealth;
+			fullBright = false;
+		}
+
+		public override void PostUpdateEquips()
+		{
+			if (Player.HeldItem.type != ModContent.ItemType<PsychoKnife>())
+			{
+				return;
+			}
+
+			if (Player.itemAnimation > 0)
+			{
+				Player.stealthTimer = 15;
+
+				if (Player.stealth > 0f)
+				{
+					Player.stealth += 0.1f;
+				}
+			}
+			else if (Player.velocity.X > -0.1 && Player.velocity.X < 0.1 && Player.velocity.Y > -0.1 && Player.velocity.Y < 0.1 && !Player.mount.Active)
+			{
+				if (Player.stealthTimer == 0 && Player.stealth > 0f)
+				{
+					Player.stealth -= 0.02f;
+					if (Player.stealth <= 0.0)
 					{
-						NetMessage.SendData(MessageID.PlayerStealth, -1, -1, null, player.whoAmI);
+						Player.stealth = 0f;
+
+						if (Main.netMode == NetmodeID.MultiplayerClient)
+						{
+							NetMessage.SendData(MessageID.PlayerStealth, -1, -1, null, Player.whoAmI);
+						}
 					}
 				}
 			}
-		}
-		else
-		{
-			if (player.stealth > 0f)
+			else
 			{
-				player.stealth += 0.1f;
+				if (Player.stealth > 0f)
+				{
+					Player.stealth += 0.1f;
+				}
+
+				if (Player.mount.Active)
+				{
+					Player.stealth = 1f;
+				}
 			}
 
-			if (player.mount.Active)
+			if (Player.stealth >= 1f)
 			{
-				player.stealth = 1f;
+				Player.stealth = 1f;
+			}
+
+			Player.GetDamage(DamageClass.Melee) += (1f - Player.stealth) * 3f;
+			Player.GetCritChance(DamageClass.Melee) += (int)((1f - Player.stealth) * 30f);
+			Player.GetKnockback(DamageClass.Melee) *= 1f + (1f - Player.stealth);
+			Player.aggro -= (int)((1f - Player.stealth) * 750f);
+
+			if (Player.stealthTimer > 0)
+			{
+				Player.stealthTimer--;
 			}
 		}
 
-		if (player.stealth != 1f)
+		public override void PostUpdateRunSpeeds()
 		{
-			player.stealth = 0;
-		}
-
-		player.GetDamage(DamageClass.Melee) += (1f - player.stealth) * 3f;
-		player.GetCritChance(DamageClass.Melee) += (int)((1f - player.stealth) * 30f);
-		player.GetKnockback(DamageClass.Melee) *= 1f + (1f - player.stealth);
-		player.aggro -= (int)((1f - player.stealth) * 750f);
-
-		if (player.stealthTimer > 0)
-		{
-			player.stealthTimer--;
+			float slowDown = Player.maxRunSpeed / 2f * (1f - Player.stealth);
+			Player.maxRunSpeed -= slowDown;
+			Player.accRunSpeed = Player.maxRunSpeed;
 		}
 	}
 }
