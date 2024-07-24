e25c332654ba31c8a3330c94140b1eb88633ea6c
GabeHasWon <gabriel@guevarameyer.com>
2024-07-24 14:46:48 -0500

    finish bows

---
+++ Content/Items/Gear/VanillaItems/Clones/Bows/Phantasm.cs
@@ -0,0 +1,26 @@
+﻿using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace PathOfTerraria.Content.Items.Gear.VanillaItems.Clones.Bows;
+
+internal class Phantasm : VanillaClone
+{
+	protected override short VanillaItemId => ItemID.Phantasm;
+
+	public override void Defaults()
+	{
+		ItemType = Core.ItemType.Ranged;
+		base.Defaults();
+	}
+
+	public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+	{
+		Projectile.NewProjectile(source, position.X, position.Y, velocity.X, velocity.Y, ProjectileID.Phantasm, damage, knockback, player.whoAmI);
+		return false;
+	}
+
+	public override bool CanConsumeAmmo(Item ammo, Player player)
+	{
+		return !Main.rand.NextBool(3);
+	}
+}
