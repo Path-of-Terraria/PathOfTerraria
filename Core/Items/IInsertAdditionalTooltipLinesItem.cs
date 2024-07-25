1aae305052291a58a217cdd585611c3916ab8fd4
Stephen T <xxlennygamerxx@gmail.com>
2024-07-25 17:29:32 -0500

    Fix typo.

--- Core/Items/IInsertAdditionalTooltipLinesItem.cs
+++ Core/Items/IInsertAdditionalTooltipLinesItem.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 
 namespace PathOfTerraria.Core.Items;
 
-public interface IInsertAdditionalTooltipLinestem
-
+public interface IInsertAdditionalTooltipLinesItem
 {
 	void InsertAdditionalTooltipLines(Item item, List<TooltipLine> tooltips, EntityModifier thisItemModifier);
 }
