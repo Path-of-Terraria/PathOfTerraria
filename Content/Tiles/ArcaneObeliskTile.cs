2ac7149655f80fcd5e41df662c00969a465ca56c
shnakamura <joaovtnakmura@gmail.com>
2024-07-30 14:13:59 -0300

    Base tile and item

---
+++ Content/Tiles/ArcaneObeliskTile.cs
@@ -0,0 +1,84 @@
+using PathOfTerraria.Content.Items.Placeable;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.GameContent.ObjectInteractions;
+using Terraria.ID;
+using Terraria.ObjectData;
+
+namespace PathOfTerraria.Content.Tiles;
+
+public class ArcaneObeliskTile : ModTile
+{
+	public override string Texture => base.Texture.Replace("Content", "Assets");
+
+	public override void SetStaticDefaults()
+	{
+		Main.tileLighted[Type] = true;
+		Main.tileLavaDeath[Type] = false;
+		Main.tileWaterDeath[Type] = false;
+		Main.tileBlockLight[Type] = true;
+		Main.tileFrameImportant[Type] = true;
+
+		TileID.Sets.HasOutlines[Type] = true;
+		
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style3x4);
+
+		TileObjectData.newTile.DrawYOffset = 2;
+		
+		TileObjectData.newTile.Height = 5;
+		TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16, 16, 16 };
+		
+		TileObjectData.newTile.Origin = Point16.Zero;
+		
+		TileObjectData.addTile(Type);
+
+		DustType = DustID.Stone;
+		HitSound = SoundID.Dig;
+		
+		AddMapEntry(new Color(142, 136, 174), CreateMapEntryName());
+	}
+
+	public override void NumDust(int i, int j, bool fail, ref int num)
+	{
+		num = fail ? 1 : 3;
+	}
+
+	public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings)
+	{
+		
+		return true;
+	}
+	
+	public override void MouseOver(int i, int j) {
+		Main.LocalPlayer.cursorItemIconEnabled = true;
+		Main.LocalPlayer.cursorItemIconID = ModContent.ItemType<ArcaneObeliskItem>();
+	}
+
+	public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+	{
+		b = 0.4f;
+		g = 0.1f;
+	}
+
+	public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+	{
+		var tile = Framing.GetTileSafely(i, j);
+
+		if (!tile.HasTile || tile.TileType != Type)
+		{
+			return;
+		}
+		
+		var texture = ModContent.Request<Texture2D>(Texture + "_Glow").Value;
+		
+		var frame = new Rectangle(tile.TileFrameX, tile.TileFrameY, 18, 18);
+
+		var data = TileObjectData.GetTileData(tile);
+
+		var offset = new Vector2(data.DrawXOffset, data.DrawYOffset);
+		var zero = Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange);
+		var position = new Vector2(i, j) * 16f - Main.screenPosition + zero + offset;
+		
+		spriteBatch.Draw(texture, position, frame, new Color(255, 255, 255, 0));
+	}
+}
