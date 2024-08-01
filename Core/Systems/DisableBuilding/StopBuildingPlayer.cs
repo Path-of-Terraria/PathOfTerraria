9eb1fdc0442648b704128fc7dce3f6a4cdf4f8ac
GabeHasWon <gabriel@guevarameyer.com>
2024-07-31 19:17:35 -0500

    finished? disabling player placement, mining and cutting system

--- Core/Systems/DisableBuilding/StopBuildingPlayer.cs
+++ Core/Systems/DisableBuilding/StopBuildingPlayer.cs
@@ -11,9 +11,17 @@ internal class StopBuildingPlayer : ModPlayer
 	/// </summary>
 	public bool ConstantStopBuilding = false;
 
+	/// <summary>
+	/// I don't know why, but <see cref="DisableMining(ILContext)"/> doesn't work due to method order. /shrug<br/>
+	/// Use this if <see cref="ConstantStopBuilding"/> doesn't work for some reason.
+	/// </summary>
+	public bool LastStopBuilding = false;
+
 	public override void Load()
 	{
 		IL_Player.PickTile += DisableMining;
+		IL_Player.PickWall += DisableMining;
+		IL_Player.ItemCheck_CutTiles += DisableMining;
 	}
 
 	private static void DisableMining(ILContext il)
@@ -22,7 +30,7 @@ internal class StopBuildingPlayer : ModPlayer
 		ILLabel label = c.DefineLabel();
 
 		c.Emit(OpCodes.Ldarg_0);
-		c.EmitDelegate((Player player) => player.GetModPlayer<StopBuildingPlayer>().ConstantStopBuilding);
+		c.EmitDelegate((Player player) => player.GetModPlayer<StopBuildingPlayer>().LastStopBuilding);
 		c.Emit(OpCodes.Brfalse, label);
 		c.Emit(OpCodes.Ret);
 		c.MarkLabel(label);
@@ -30,14 +38,15 @@ internal class StopBuildingPlayer : ModPlayer
 
 	public override void ResetEffects()
 	{
+		LastStopBuilding = ConstantStopBuilding;
 		ConstantStopBuilding = false;
 	}
 
 	public override bool CanUseItem(Item item)
 	{
-		if (item.createTile >= TileID.Dirt || item.createWall > WallID.None)
+		if (item.createTile >= TileID.Dirt || item.createWall > WallID.None || item.type == ItemID.IceRod || item.tileWand >= 0)
 		{
-			return !ConstantStopBuilding;
+			return !LastStopBuilding;
 		}
 
 		return true;
---
+++ Core/Systems/DisableBuilding/StopCuttingProjectile.cs
@@ -0,0 +1,25 @@
+﻿namespace PathOfTerraria.Core.Systems.DisableBuilding;
+
+internal class StopCuttingProjectile : GlobalProjectile
+{
+	public override bool? CanCutTiles(Projectile projectile)
+	{
+		if (projectile.owner != 255)
+		{
+			return Main.player[projectile.owner].GetModPlayer<StopBuildingPlayer>().LastStopBuilding ? false : null;
+		}
+
+		return base.CanCutTiles(projectile);
+	}
+
+	public override bool PreKill(Projectile projectile, int timeLeft)
+	{
+		if (projectile.owner != 255 && Main.player[projectile.owner].GetModPlayer<StopBuildingPlayer>().LastStopBuilding)
+		{
+			// This stops the Sandgun from dropping sand everywhere
+			projectile.noDropItem = true;
+		}
+
+		return true;
+	}
+}
