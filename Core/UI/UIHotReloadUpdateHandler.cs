2df460a264dd2a1115070006212973f4bf0be7b0
shnakamura <joaovtnakmura@gmail.com>
2024-08-07 22:51:05 -0300

    UI hot reloading

---
+++ Core/UI/UIHotReloadUpdateHandler.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Reflection.Metadata;
+using JetBrains.Annotations;
+using PathOfTerraria.Core.UI;
+using Terraria.UI;
+
+[assembly: MetadataUpdateHandler(typeof(UIHotReloadUpdateHandler))]
+
+namespace PathOfTerraria.Core.UI;
+
+internal static class UIHotReloadUpdateHandler
+{        
+	internal static void UpdateApplication(Type[]? updatedTypes)
+	{	
+		Main.QueueMainThreadAction(
+			() =>
+			{
+				foreach (Type type in updatedTypes)
+				{
+					if (!typeof(UIState).IsAssignableFrom(type))
+					{
+						continue;
+					}
+					
+					var methodInfo = typeof(UIManager).GetMethod("RefreshStates", BindingFlags.NonPublic | BindingFlags.Static);
+					var generatedMethodInfo = methodInfo?.MakeGenericMethod(type);
+					
+					generatedMethodInfo?.Invoke(null, null);
+				}
+			}
+		);
+	}
+}
