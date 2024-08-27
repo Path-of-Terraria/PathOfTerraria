1d33c0e336588cb694389e9589ba22fb355d98f3
shnakamura <joaovtnakmura@gmail.com>
2024-08-27 19:08:09 -0300

    Implementation of Ale, Whisky & Pool Table

---
+++ Content/Tiles/Town/PoolTable.cs
@@ -0,0 +1,24 @@
+using Terraria.ID;
+using Terraria.ObjectData;
+
+namespace PathOfTerraria.Content.Tiles.Town;
+
+public class PoolTable : ModTile
+{
+	public override void SetStaticDefaults()
+	{
+		Main.tileFrameImportant[Type] = true;
+		Main.tileLavaDeath[Type] = true;
+
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+		TileObjectData.addTile(Type);
+
+		DustType = DustID.WoodFurniture;
+		HitSound = SoundID.Dig;
+	}
+	
+	public override void NumDust(int i, int j, bool fail, ref int num)
+	{
+		num = fail ? 1 : 3;
+	}
+}
