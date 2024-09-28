2af13fe850d66dceda7e7ea51e25b9266d55123c
Collin Herber <rustaddicts@gmail.com>
2024-09-28 17:06:11 -0400

    chore: add simple check for points before subtracting 0 a bunch

--- Common/Systems/PassiveTreeSystem/PassiveTreeSystem.cs
+++ Common/Systems/PassiveTreeSystem/PassiveTreeSystem.cs
@@ -86,7 +86,7 @@ internal class PassiveTreePlayer : ModPlayer
 				}
 			}
 
-			if (passive.InternalIdentifier != "Anchor")
+			if (passive.InternalIdentifier != "Anchor" && passive.Level > 0)
 			{
 				Points -= passive.Level;
 			}
