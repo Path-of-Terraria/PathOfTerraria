bc062e656bdeacae9128c8291b3d819f65b8eb4b
GabeHasWon <gabriel@guevarameyer.com>
2024-09-30 13:14:45 -0500

    add clump tile, placer, update structures

---
+++ Content/Tiles/BossDomain/RoyalHoneyClumpTile.cs
@@ -0,0 +1,28 @@
+﻿using Terraria.ID;
+
+namespace PathOfTerraria.Content.Tiles.BossDomain;
+
+internal class RoyalHoneyClumpTile : ModTile
+{
+	public override void SetStaticDefaults()
+	{
+		Main.tileCut[Type] = true;
+		Main.tileSolid[Type] = false;
+		Main.tileFrameImportant[Type] = true;
+
+		DustType = DustID.Honey;
+
+		AddMapEntry(new Color(255, 156, 12));
+	}
+
+	public override void HitWire(int i, int j)
+	{
+		Wiring.SkipWire(i, j);
+		Tile tile = Main.tile[i, j];
+
+		if (tile.TileFrameY == 0)
+		{
+			tile.TileFrameY = 0;
+		}
+	}
+}
