b552af5aeb9c5d47b20eafcab5af1239ff031128
GabeHasWon <gabriel@guevarameyer.com>
2024-10-10 13:44:38 -0500

    remove unused method, update docs

--- Common/NPCs/Components/NPCComponentExtensions.cs
+++ Common/NPCs/Components/NPCComponentExtensions.cs
@@ -6,6 +6,13 @@ namespace PathOfTerraria.Common.NPCs.Components;
 // of convenience when using NPC components.
 public static class NPCComponentExtensions
 {
+	/// <summary>
+	/// Tries to enable the given <typeparamref name="T"/> component on the current entity. This will not run on servers.
+	/// </summary>
+	/// <typeparam name="T">The component to enable.</typeparam>
+	/// <param name="npc">The NPC to enable the component on.</param>
+	/// <param name="initializer">Optional initialization, such as setting parameters, for the component.</param>
+	/// <returns>Whether the component was successfully enabled.</returns>
 	public static bool TryEnableComponent<T>(this NPC npc, Action<T>? initializer = null) where T : NPCComponent
 	{
 		if (Main.netMode == NetmodeID.Server)
@@ -20,25 +27,6 @@ public static class NPCComponentExtensions
 
 		component.Enabled = true;
 		
-		initializer?.Invoke(component);
-
-		return true;
-	}
-
-	public static bool TryEnableClientComponent<T>(this NPC npc, Action<T>? initializer = null) where T : NPCComponent
-	{
-		if (Main.netMode == NetmodeID.Server)
-		{
-			return false;
-		}
-
-		if (!npc.TryGetGlobalNPC(out T? component))
-		{
-			return false;
-		}
-
-		component.Enabled = true;
-
 		initializer?.Invoke(component);
 
 		return true;
