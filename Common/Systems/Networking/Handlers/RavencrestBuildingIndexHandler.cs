d1976335c98fc9d534eb032090634926a6d676b2
GabeHasWon <gabriel@guevarameyer.com>
2024-10-16 16:42:40 -0500

    update building handler doc

--- Common/Systems/Networking/Handlers/RavencrestBuildingIndexHandler.cs
+++ Common/Systems/Networking/Handlers/RavencrestBuildingIndexHandler.cs
@@ -1,5 +1,4 @@
 using PathOfTerraria.Common.Subworlds.RavencrestContent;
-using PathOfTerraria.Common.Systems.Experience;
 using System.IO;
 
 namespace PathOfTerraria.Common.Systems.Networking.Handlers;
@@ -7,13 +6,11 @@ namespace PathOfTerraria.Common.Systems.Networking.Handlers;
 internal static class RavencrestBuildingIndex
 {
 	/// <summary>
-	/// Spawns an orb on all clients but <paramref name="target"/>. 
-	/// Generally, this should only be called by <see cref="ExperienceModSystem.SpawnExperience(int, Vector2, Vector2, int, bool)"/>.
+	/// Sets the building index of the given Ravencrest building.
+	/// This should be called instead of <see cref="RavencrestSystem.UpgradeBuilding(string, int)"/> on multiplayer clients.
 	/// </summary>
-	/// <param name="target">Target for the exp to aim for.</param>
-	/// <param name="xpValue">Amount of XP to spawn.</param>
-	/// <param name="position">Where it spawns.</param>
-	/// <param name="velocity">The base velocity of the spawned exp.</param>
+	/// <param name="name">Name of the building.</param>
+	/// <param name="index">Index to use.</param>
 	public static void Send(string name, int index)
 	{
 		ModPacket packet = Networking.GetPacket(Networking.Message.SetRavencrestBuildingIndex);
