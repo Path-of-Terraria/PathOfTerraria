641861d70e42480dc16f6bee54c2b0422d4a905e
GabeHasWon <gabriel@guevarameyer.com>
2024-10-23 20:25:10 -0500

    create all wands, cleanup & boomerang fixes

---
+++ Content/Items/Gear/Weapons/Wand/EbonwoodWand.cs
@@ -0,0 +1,63 @@
+﻿using PathOfTerraria.Core.Items;
+using Terraria.ID;
+
+namespace PathOfTerraria.Content.Items.Gear.Weapons.Wand;
+
+internal class EbonwoodWand : Wand
+{
+	public override void SetStaticDefaults()
+	{
+		base.SetStaticDefaults();
+
+		PoTStaticItemData staticData = this.GetStaticData();
+		staticData.MinDropItemLevel = 12;
+	}
+
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
+
+		Item.damage = 20;
+		Item.mana = 5;
+		Item.useTime = Item.useAnimation = 40;
+		Item.shoot = ModContent.ProjectileType<EbonwoodWandProjectile>();
+		Item.shootSpeed = 10;
+		Item.UseSound = SoundID.Item7;
+	}
+
+	public class EbonwoodWandProjectile : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			Projectile.Size = new(20);
+			Projectile.friendly = true;
+			Projectile.penetrate = 2;
+			Projectile.timeLeft = 250;
+		}
+
+		public override void AI()
+		{
+			Projectile.velocity.Y += 0.1f;
+			Projectile.rotation += Projectile.velocity.X * 0.04f;
+
+			if (Projectile.timeLeft < 20)
+			{
+				Projectile.velocity *= 0.95f;
+				Projectile.Opacity = Projectile.timeLeft / 20f;
+			}
+
+			if (Main.rand.NextBool(32))
+			{
+				Gore.NewGore(Projectile.GetSource_FromAI(), Projectile.Center, Vector2.Zero, GoreID.TreeLeaf_Corruption);
+			}
+		}
+
+		public override void OnKill(int timeLeft)
+		{
+			for (int i = 0; i < 20; ++i)
+			{
+				Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Corruption, Projectile.velocity.X * 0.25f, Projectile.velocity.Y * 0.25f);
+			}
+		}
+	}
+}
---
+++ Content/Items/Gear/Weapons/Wand/ShadewoodWand.cs
@@ -0,0 +1,63 @@
+﻿using PathOfTerraria.Core.Items;
+using Terraria.ID;
+
+namespace PathOfTerraria.Content.Items.Gear.Weapons.Wand;
+
+internal class ShadewoodWand : Wand
+{
+	public override void SetStaticDefaults()
+	{
+		base.SetStaticDefaults();
+
+		PoTStaticItemData staticData = this.GetStaticData();
+		staticData.MinDropItemLevel = 12;
+	}
+
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
+
+		Item.damage = 14;
+		Item.mana = 3;
+		Item.useTime = Item.useAnimation = 22;
+		Item.shoot = ModContent.ProjectileType<ShadewoodWandProjectile>();
+		Item.shootSpeed = 12;
+		Item.UseSound = SoundID.Item7;
+	}
+
+	public class ShadewoodWandProjectile : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			Projectile.Size = new(20);
+			Projectile.friendly = true;
+			Projectile.penetrate = 1;
+			Projectile.timeLeft = 250;
+		}
+
+		public override void AI()
+		{
+			Projectile.velocity.Y += 0.05f;
+			Projectile.rotation += Projectile.velocity.X * 0.055f;
+
+			if (Projectile.timeLeft < 20)
+			{
+				Projectile.velocity *= 0.97f;
+				Projectile.Opacity = Projectile.timeLeft / 20f;
+			}
+
+			if (Main.rand.NextBool(32))
+			{
+				Gore.NewGore(Projectile.GetSource_FromAI(), Projectile.Center, Vector2.Zero, GoreID.TreeLeaf_Crimson);
+			}
+		}
+
+		public override void OnKill(int timeLeft)
+		{
+			for (int i = 0; i < 20; ++i)
+			{
+				Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.CrimsonPlants, Projectile.velocity.X * 0.25f, Projectile.velocity.Y * 0.25f);
+			}
+		}
+	}
+}
