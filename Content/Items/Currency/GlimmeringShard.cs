2e5e6b694c12d394d35fa31046ff0bf56efdc0cc
GabeHasWon <gabriel@guevarameyer.com>
2024-10-28 10:27:11 -0500

    make glimmering shard

---
+++ Content/Items/Currency/GlimmeringShard.cs
@@ -0,0 +1,29 @@
+﻿using PathOfTerraria.Core.Items;
+using Terraria.ID;
+
+namespace PathOfTerraria.Content.Items.Currency;
+
+internal class GlimmeringShard : ModItem
+{
+	public override void SetDefaults()
+	{
+		Item.CloneDefaults(ItemID.Silk);
+		Item.Size = new Vector2(30, 28);
+		Item.rare = ItemRarityID.Green;
+	}
+
+	private class GlimmeringShardRightClickCheck : GlobalItem
+	{
+		public override void RightClick(Item item, Player player)
+		{
+			item.stack++;
+
+			if (player.HeldItem.type == ModContent.ItemType<GlimmeringShard>() && item.TryGetGlobalItem(out PoTGlobalItem potGlobal) 
+				&& item.GetInstanceData().Rarity == Common.Enums.ItemRarity.Magic)
+			{
+				item.GetInstanceData().Affixes.Clear();
+				PoTItemHelper.Roll(item, item.GetInstanceData().RealLevel);
+			}
+		}
+	}
+}
