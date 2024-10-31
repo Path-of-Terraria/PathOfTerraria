452de55d6d3ab9e7021f259f146d40336636bcd8
GabeHasWon <gabriel@guevarameyer.com>
2024-10-31 12:24:06 -0500

    finish global loot

--- Common/NPCs/GlobalLoot.cs
+++ Common/NPCs/GlobalLoot.cs
@@ -1,4 +1,5 @@
-﻿using Terraria.GameContent.ItemDropRules;
+﻿using PathOfTerraria.Content.Items.Consumables.Maps;
+using Terraria.GameContent.ItemDropRules;
 
 namespace PathOfTerraria.Common.NPCs;
 
@@ -6,6 +7,7 @@ internal class GlobalLoot : GlobalNPC
 {
 	public override void ModifyGlobalLoot(Terraria.ModLoader.GlobalLoot globalLoot)
 	{
-		//globalLoot.Add(ItemDropRule.ByCondition(new Conditions.King));
+		globalLoot.Add(ItemDropRule.ByCondition(new BossDownedCondition(BossDownedCondition.Bosses.KingSlime), ModContent.ItemType<KingSlimeMap>(), 1000));
+		globalLoot.Add(ItemDropRule.ByCondition(new BossDownedCondition(BossDownedCondition.Bosses.EyeOfCthulhu), ModContent.ItemType<EoCMap>(), 1000));
 	}
 }
