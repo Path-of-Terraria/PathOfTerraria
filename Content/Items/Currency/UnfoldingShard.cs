9faeffdb0901dcf1d8e1f32e47f043add1b355fa
Collin Herber <rustaddicts@gmail.com>
2024-11-01 21:12:26 -0400

    feat(#446): add in the glimmering shard

---
+++ Content/Items/Currency/UnfoldingShard.cs
@@ -0,0 +1,19 @@
+﻿using PathOfTerraria.Common.Enums;
+using PathOfTerraria.Core.Items;
+
+namespace PathOfTerraria.Content.Items.Currency;
+
+internal class UnfoldingShard : CurrencyShard
+{
+	public override bool CanRightClick()
+	{
+		return base.CanRightClick() && Main.LocalPlayer.HeldItem.GetInstanceData().Rarity == ItemRarity.Normal;
+	}
+
+	public override void RightClick(Player player)
+	{
+		PoTInstanceItemData data = player.HeldItem.GetInstanceData();
+		data.Rarity = ItemRarity.Magic;
+		PoTItemHelper.Roll(player.HeldItem, data.RealLevel);
+	}
+}
