f2208222b07c4f1dbc57df944e1934cc634bbdb4
GabeHasWon <gabriel@guevarameyer.com>
2024-11-01 20:08:57 -0500

    pathfinding, morven NPC

---
+++ Common/NPCs/Pathfinding/Pathfinder.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Terraria.DataStructures;
+
+namespace PathOfTerraria.Common.NPCs.Pathfinding;
+
+internal class Pathfinder
+{
+	public enum Direction : byte
+	{
+		None,
+		Above,
+		Below,
+		Left,
+		Right
+	}
+
+	public readonly record struct FoundPoint(Point16 Position, Direction Direction);
+	public readonly record struct WorkingPoint(Direction Direction, float Weight);
+
+	public bool HasPath { get; private set; }
+	
+	public ReadOnlyCollection<FoundPoint> Path { get; private set; }
+
+	readonly Dictionary<Point16, WorkingPoint> found = [];
+	readonly PriorityQueue<Point16, float> frontier = new();
+	readonly List<FoundPoint> path = [];
+
+	public bool DrawPath(Point16 start, Point16 end)
+	{
+		path.Clear();
+		found.Clear();
+		frontier.Clear();
+
+		if (Solid(start) || Solid(end))
+		{
+			return false;
+		}
+
+		AddPoint(end, start, Direction.None, 0);
+
+		while (frontier.Count > 0)
+		{
+			Point16 pos = frontier.Dequeue();
+
+			if (pos == end)
+			{
+				BuildPath(start, end);
+				HasPath = true;
+				return true;
+			}
+
+			AddSurrounds(end, pos, found[pos].Weight);
+		}
+
+		return false;
+	}
+
+	private void BuildPath(Point16 start, Point16 end)
+	{
+		Point16 pos = end;
+
+		while (pos != start)
+		{
+			Direction dir = found[pos].Direction;
+			path.Add(new FoundPoint(pos, dir));
+
+			Point16 direction = ToVector(dir);
+			pos = new Point16(pos.X + direction.X, pos.Y + direction.Y);
+		}
+
+		Path = new(path);
+	}
+
+	public static Point16 ToVector(Direction direction)
+	{
+		return direction switch
+		{
+			Direction.Left => new Point16(-1, 0),
+			Direction.Right => new Point16(1, 0),
+			Direction.Below => new Point16(0, 1),
+			Direction.Above => new Point16(0, -1),
+			_ => Point16.Zero
+		};
+	}
+
+	public static bool Solid(Point16 position)
+	{
+		return WorldGen.SolidOrSlopedTile(position.X, position.Y);
+	}
+
+	public bool SolidOrInvalid(Point16 position)
+	{
+		return Solid(position) || found.ContainsKey(position);
+	}
+
+	private void AddSurrounds(Point16 end, Point16 pos, float currentWeight)
+	{
+		if (!SolidOrInvalid(new Point16(pos.X, pos.Y + 1)))
+		{
+			AddPoint(end, new Point16(pos.X, pos.Y + 1), Direction.Above, currentWeight);
+		}
+
+		if (!SolidOrInvalid(new Point16(pos.X, pos.Y - 1)))
+		{
+			AddPoint(end, new Point16(pos.X, pos.Y - 1), Direction.Below, currentWeight);
+		}
+
+		if (!SolidOrInvalid(new Point16(pos.X + 1, pos.Y)))
+		{
+			AddPoint(end, new Point16(pos.X + 1, pos.Y), Direction.Left, currentWeight);
+		}
+
+		if (!SolidOrInvalid(new Point16(pos.X - 1, pos.Y)))
+		{
+			AddPoint(end, new Point16(pos.X - 1, pos.Y), Direction.Right, currentWeight);
+		}
+	}
+
+	private void AddPoint(Point16 end, Point16 position, Direction direction, float currentWeight)
+	{
+		found.Add(position, new WorkingPoint(direction, currentWeight));
+		frontier.Enqueue(position, currentWeight + SquaredDistance(end, position));
+	}
+
+	private static float Distance(Point16 start, Point16 position)
+	{
+		return MathF.Sqrt(MathF.Pow(position.Y - start.Y, 2) + MathF.Pow(position.X - start.X, 2));
+	}
+
+	private static float SquaredDistance(Point16 start, Point16 position)
+	{
+		return MathF.Pow(position.Y - start.Y, 2) + MathF.Pow(position.X - start.X, 2);
+	}
+}
---
+++ Content/NPCs/Town/MorvenNPC.cs
@@ -0,0 +1,145 @@
+using PathOfTerraria.Common.NPCs.Components;
+using PathOfTerraria.Common.NPCs.Effects;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.Localization;
+using PathOfTerraria.Common.Utilities.Extensions;
+using PathOfTerraria.Common.Systems.Questing;
+using PathOfTerraria.Common.Systems.Questing.Quests.MainPath;
+using PathOfTerraria.Common.NPCs;
+using PathOfTerraria.Common.NPCs.OverheadDialogue;
+using PathOfTerraria.Common.NPCs.Dialogue;
+
+namespace PathOfTerraria.Content.NPCs.Town;
+
+[AutoloadHead]
+public sealed class MorvenNPC : ModNPC, IQuestMarkerNPC, IOverheadDialogueNPC
+{
+	OverheadDialogueInstance IOverheadDialogueNPC.CurrentDialogue { get; set; }
+
+	private float animCounter;
+
+	public override void SetStaticDefaults()
+	{
+		Main.npcFrameCount[NPC.type] = 25;
+
+		NPCID.Sets.ExtraFramesCount[NPC.type] = 9;
+		NPCID.Sets.AttackFrameCount[NPC.type] = 4;
+		NPCID.Sets.DangerDetectRange[NPC.type] = 500;
+		NPCID.Sets.AttackType[NPC.type] = 1;
+		NPCID.Sets.AttackTime[NPC.type] = 16;
+		NPCID.Sets.AttackAverageChance[NPC.type] = 30;
+		NPCID.Sets.NoTownNPCHappiness[Type] = true;
+	}
+
+	public override void SetDefaults()
+	{
+		NPC.CloneDefaults(NPCID.ArmsDealer);
+		NPC.townNPC = true;
+		NPC.friendly = true;
+		NPC.aiStyle = 7;
+		NPC.defense = 30;
+		NPC.lifeMax = 250;
+		NPC.HitSound = SoundID.NPCHit1;
+		NPC.DeathSound = SoundID.NPCDeath1;
+		NPC.knockBackResist = 0.5f;
+		AnimationType = NPCID.Guide;
+		
+		NPC.TryEnableComponent<NPCHitEffects>(
+			c =>
+			{
+				c.AddGore(new($"{PoTMod.ModName}/{Name}_0", 1));
+				c.AddGore(new($"{PoTMod.ModName}/{Name}_1", 2));
+				c.AddGore(new($"{PoTMod.ModName}/{Name}_2", 2));
+				
+				c.AddDust(new(DustID.Blood, 20));
+			}
+		);
+
+		NPC.TryEnableComponent<NPCTownDialogue>(
+			c =>
+			{
+				c.AddDialogue(new NPCTownDialogue.DialogueEntry($"Mods.{PoTMod.ModName}.NPCs.{Name}.Dialogue.Common0"));
+				c.AddDialogue(new NPCTownDialogue.DialogueEntry($"Mods.{PoTMod.ModName}.NPCs.{Name}.Dialogue.Common1"));
+				c.AddDialogue(new NPCTownDialogue.DialogueEntry($"Mods.{PoTMod.ModName}.NPCs.{Name}.Dialogue.Common2"));
+				c.AddDialogue(new NPCTownDialogue.DialogueEntry($"Mods.{PoTMod.ModName}.NPCs.{Name}.Dialogue.Common3"));
+			}
+		);
+	}
+
+	public override void TownNPCAttackStrength(ref int damage, ref float knockback)
+	{
+		damage = 13;
+		knockback = 3f;
+	}
+
+	public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown)
+	{
+		cooldown = 5;
+		randExtraCooldown = 5;
+	}
+
+	public override void TownNPCAttackProj(ref int projType, ref int attackDelay)
+	{
+		projType = 507;
+		attackDelay = 1;
+	}
+
+	public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset)
+	{
+		multiplier = 11f;
+		randomOffset = 2f;
+	}
+
+	public override ITownNPCProfile TownNPCProfile()
+	{
+		return this.GetDefaultProfile();
+	}
+
+	public override void SetChatButtons(ref string button, ref string button2)
+	{
+		button = Language.GetTextValue("LegacyInterface.28");
+		button2 = !ModContent.GetInstance<EoCQuest>().CanBeStarted ? "" : Language.GetTextValue("Mods.PathOfTerraria.NPCs.Quest");
+	}
+
+	public override void OnChatButtonClicked(bool firstButton, ref string shopName)
+	{
+		if (firstButton)
+		{
+			shopName = "Shop";
+		}
+		else
+		{
+			Main.npcChatText = Language.GetTextValue("Mods.PathOfTerraria.NPCs.EldricNPC.Dialogue.Quest");
+			Main.LocalPlayer.GetModPlayer<QuestModPlayer>().StartQuest($"{PoTMod.ModName}/{nameof(EoCQuest)}");
+		}
+	}
+
+	public override void FindFrame(int frameHeight)
+	{
+		if (!NPC.IsABestiaryIconDummy)
+		{
+			return;
+		}
+
+		animCounter += 0.25f;
+
+		if (animCounter >= 16)
+		{
+			animCounter = 2;
+		}
+		else if (animCounter < 2)
+		{
+			animCounter = 2;
+		}
+
+		int frame = (int)animCounter;
+		NPC.frame.Y = frame * frameHeight;
+	}
+
+	public bool HasQuestMarker(out Quest quest)
+	{
+		quest = ModContent.GetInstance<EoCQuest>();
+		return !quest.Completed;
+	}
+}
