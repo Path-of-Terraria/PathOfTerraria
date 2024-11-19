8419af4ca8413b47d56a238df3db351cfb2f3907
GabeHasWon <gabriel@guevarameyer.com>
2024-11-18 22:11:21 -0600

    map

---
+++ Content/Items/Consumables/Maps/BossMaps/DeerclopsMap.cs
@@ -0,0 +1,36 @@
+﻿using PathOfTerraria.Common.Subworlds.BossDomains;
+using PathOfTerraria.Core.Items;
+using SubworldLibrary;
+using Terraria.Localization;
+
+namespace PathOfTerraria.Content.Items.Consumables.Maps.BossMaps;
+
+internal class DeerclopsMap : Map
+{
+	public override int MaxUses => GetBossUseCount();
+
+	public override void SetStaticDefaults()
+	{
+		base.SetStaticDefaults();
+
+		PoTStaticItemData staticData = this.GetStaticData();
+		staticData.DropChance = 1f;
+	}
+
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
+
+		Item.Size = new Vector2(30, 30);
+	}
+
+	public override void OpenMap()
+	{
+		SubworldSystem.Enter<DeerclopsDomain>();
+	}
+
+	public override string GenerateName(string defaultName)
+	{
+		return Language.GetTextValue($"Mods.{PoTMod.ModName}.Items.{Name}.DisplayName");
+	}
+}
