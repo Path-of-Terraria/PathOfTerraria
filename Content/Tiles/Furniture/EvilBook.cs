5bf9f425c954dc2a87a3eaf7d67ec7aaa4ac1076
GabeHasWon <gabriel@guevarameyer.com>
2024-11-27 12:47:40 -0600

    evil book tile entity

--- Content/Tiles/Furniture/EvilBook.cs
+++ Content/Tiles/Furniture/EvilBook.cs
@@ -17,6 +17,7 @@ internal class EvilBook : ModTile
 
 		TileObjectData.newTile.CopyFrom(TileObjectData.StyleOnTable1x1);
 		TileObjectData.newTile.RandomStyleRange = 3;
+		TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(ModContent.GetInstance<EvilBookEntity>().Hook_AfterPlacement, -1, 0, false);
 		TileObjectData.newTile.StyleHorizontal = true;
 		TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidWithTop | AnchorType.SolidTile | AnchorType.Table 
 			| AnchorType.Platform, 1, 0);
@@ -28,7 +29,47 @@ internal class EvilBook : ModTile
 		RegisterItemDrop(ModContent.ItemType<AncientEvilBook>());
 	}
 
-	public override void NearbyEffects(int i, int j, bool closer)
+	public override void NumDust(int i, int j, bool fail, ref int num)
+	{
+		num = 3;
+	}
+
+	public override void SetSpriteEffects(int i, int j, ref SpriteEffects effects)
+	{
+		effects = (i % 2 == 0) ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+	}
+}
+
+internal class EvilBookEntity : ModTileEntity
+{
+	public override bool IsTileValidForEntity(int x, int y)
+	{
+		return Main.tile[x, y].HasTile && Main.tile[x, y].TileType == ModContent.TileType<EvilBook>();
+	}
+
+	public override int Hook_AfterPlacement(int i, int j, int type, int style, int direction, int alternate)
+	{
+		Tile tile = Main.tile[i, j];
+		i -= tile.TileFrameX / 18;
+		j -= tile.TileFrameY / 18 % 5;
+
+		if (Main.netMode == NetmodeID.MultiplayerClient)
+		{
+			NetMessage.SendTileSquare(Main.myPlayer, i, j, 3, 5);
+			NetMessage.SendData(MessageID.TileEntityPlacement, number: i, number2: j, number3: Type);
+			return -1;
+		}
+
+		int placedEntity = Place(i, j);
+		return placedEntity;
+	}
+
+	public override void Update()
+	{
+		CheckRitual();
+	}
+
+	private void CheckRitual()
 	{
 		foreach (Projectile proj in Main.ActiveProjectiles)
 		{
@@ -37,6 +78,9 @@ internal class EvilBook : ModTile
 				return;
 			}
 		}
+
+		int i = Position.X;
+		int j = Position.Y;
 
 		Tile leftCandle = Main.tile[i - 1, j];
 		Tile rightCandle = Main.tile[i + 1, j];
@@ -47,7 +91,7 @@ internal class EvilBook : ModTile
 			return;
 		}
 
-		if (!IsValidCandle(leftCandle) || !IsValidCandle(rightCandle) || bar.TileType != TileID.MetalBars || 
+		if (!IsValidCandle(leftCandle) || !IsValidCandle(rightCandle) || bar.TileType != TileID.MetalBars ||
 			bar.TileFrameX != 144 && bar.TileFrameX != 342)
 		{
 			return;
@@ -55,22 +99,18 @@ internal class EvilBook : ModTile
 
 		Vector2 worldPos = new Vector2(i, j).ToWorldCoordinates();
 		IEntitySource source = new EntitySource_TileInteraction(Main.player[Player.FindClosest(worldPos - new Vector2(8), 16, 16)], i, j);
-		Projectile.NewProjectileDirect(source, worldPos, Vector2.Zero, ModContent.ProjectileType<SkeletronRitualProj>(), 0, 0, Main.myPlayer);
+		int newProj = Projectile.NewProjectile(source, worldPos, Vector2.Zero, ModContent.ProjectileType<SkeletronRitualProj>(), 0, 0, Main.myPlayer);
+
+		if (Main.netMode == NetmodeID.Server)
+		{
+			NetMessage.SendData(MessageID.SyncProjectile, -1, -1, null, newProj);
+		}
+
 		return;
 
 		static bool IsValidCandle(Tile tile)
 		{
 			return tile.TileType == TileID.Candles || tile.TileType == TileID.PlatinumCandle;
 		}
-	}
-
-	public override void NumDust(int i, int j, bool fail, ref int num)
-	{
-		num = 3;
-	}
-
-	public override void SetSpriteEffects(int i, int j, ref SpriteEffects effects)
-	{
-		effects = (i % 2 == 0) ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
 	}
 }
