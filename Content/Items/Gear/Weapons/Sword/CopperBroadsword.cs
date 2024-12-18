e6ab10c1550c582c23d8cfe3056110564cf5a9d9
GabeHasWon <gabriel@guevarameyer.com>
2024-12-18 13:53:00 -0600

    fix some swords not inheriting from broadsword

--- Content/Items/Gear/Weapons/Sword/CopperBroadsword.cs
+++ Content/Items/Gear/Weapons/Sword/CopperBroadsword.cs
@@ -4,7 +4,7 @@ using Terraria.ID;
 
 namespace PathOfTerraria.Content.Items.Gear.Weapons.Sword;
 
-internal class CopperBroadsword : Sword
+internal class CopperBroadsword : Broadsword
 {
 	public override void SetStaticDefaults()
 	{
