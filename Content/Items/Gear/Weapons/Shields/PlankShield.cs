7956436c5019556c309b7fb3ca52298b46f7f124
GabeHasWon <gabriel@guevarameyer.com>
2024-12-19 22:51:25 -0600

    start shield

---
+++ Content/Items/Gear/Weapons/Shields/PlankShield.cs
@@ -0,0 +1,10 @@
+﻿namespace PathOfTerraria.Content.Items.Gear.Weapons.Shields;
+
+internal class PlankShield : Shield
+{
+	public override void SetDefaults()
+	{
+		Item.Size = new(18, 24);
+		Item.value = Item.buyPrice(0, 0, 3, 0);
+	}
+}
---
+++ Content/Items/Gear/Weapons/Shields/Shield.cs
@@ -0,0 +1,10 @@
+﻿using PathOfTerraria.Content.Items.Gear.Offhands;
+
+namespace PathOfTerraria.Content.Items.Gear.Weapons.Shields;
+
+internal abstract class Shield : Offhand
+{
+	public override void SetDefaults()
+	{
+	}
+}
