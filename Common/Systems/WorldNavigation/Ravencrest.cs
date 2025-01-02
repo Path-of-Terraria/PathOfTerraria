46b4d7ca958b0579b7642346202e9a1db131c401
Collin Herber <rustaddicts@gmail.com>
2025-01-02 17:35:14 -0500

    fix(#598): Fixes an issue with old world navigation code

--- Common/Systems/WorldNavigation/Ravencrest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PathOfTerraria.Common.Systems.WorldNavigation;
-
-public class Ravencrest : WorldNavigation
-{
-	protected override string WorldFileName => "Ravencrest";
-	public override Vector2 SpawnPosition => new(-350, 130);
-}
