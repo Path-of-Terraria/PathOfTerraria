c03819c240d33471447352c883ec0bc9ec06f697
GabeHasWon <gabriel@guevarameyer.com>
2025-01-06 12:23:04 -0600

    add ravencrest background, background position system

---
+++ Content/Scenes/RavencrestSurfaceBackground.cs
@@ -0,0 +1,52 @@
+﻿using PathOfTerraria.Common.Systems.SurfaceBGModifications;
+
+namespace PathOfTerraria.Content.Scenes;
+
+internal class RavencrestSurfaceBackground : ModSurfaceBackgroundStyle, IBackgroundModifier
+{
+	public override int ChooseMiddleTexture()
+	{
+		return BackgroundTextureLoader.GetBackgroundSlot(Mod, "Assets/Backgrounds/RavencrestMiddle");
+	}
+
+	public override int ChooseFarTexture()
+	{
+		return BackgroundTextureLoader.GetBackgroundSlot(Mod, "Assets/Backgrounds/RavencrestFar");
+	}
+
+	public override int ChooseCloseTexture(ref float scale, ref double parallax, ref float a, ref float b)
+	{
+		b -= 1500;
+		return BackgroundTextureLoader.GetBackgroundSlot(Mod, "Assets/Backgrounds/RavencrestFront");
+	}
+
+	public override void ModifyFarFades(float[] fades, float transitionSpeed)
+	{
+		for (int i = 0; i < fades.Length; i++)
+		{
+			if (i == Slot)
+			{
+				fades[i] += transitionSpeed;
+
+				if (fades[i] > 1f)
+				{
+					fades[i] = 1f;
+				}
+			}
+			else
+			{
+				fades[i] -= transitionSpeed;
+
+				if (fades[i] < 0f)
+				{
+					fades[i] = 0f;
+				}
+			}
+		}
+	}
+
+	public void ModifyPosition(bool isFar, ref int y)
+	{
+		y -= isFar ? 1100 : 1150;
+	}
+}
