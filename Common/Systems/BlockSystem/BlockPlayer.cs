d941e67625adf6cbb90df399ac0fa2388799ef75
GabeHasWon <gabriel@guevarameyer.com>
2025-01-07 13:00:42 -0600

    add block player

---
+++ Common/Systems/BlockSystem/BlockPlayer.cs
@@ -0,0 +1,30 @@
+﻿namespace PathOfTerraria.Common.Systems.BlockSystem;
+
+internal class BlockPlayer : ModPlayer
+{
+	public const float DefaultMaxBlockChance = 0.4f;
+
+	public float BlockChance { get; private set; } = 0;
+	public float MaxBlockChance { get; private set; } = DefaultMaxBlockChance;
+
+	public override void ResetEffects()
+	{
+		BlockChance = 0;
+		MaxBlockChance = DefaultMaxBlockChance;
+	}
+
+	public void AddBlockChance(float add)
+	{
+		BlockChance += add;
+
+		if (BlockChance > MaxBlockChance)
+		{
+			BlockChance = MaxBlockChance;
+		}
+	}
+
+	public override bool FreeDodge(Player.HurtInfo info)
+	{
+		return Main.rand.NextFloat() < BlockChance;
+	}
+}
