3478a163c269f556ac0d5d9d150a9d6c4c66df1b
GabeHasWon <gabriel@guevarameyer.com>
2025-01-08 14:23:41 -0600

    start attributes

---
+++ Common/Systems/ModPlayers/AttributesPlayer.cs
@@ -0,0 +1,13 @@
+﻿namespace PathOfTerraria.Common.Systems.ModPlayers;
+
+internal class AttributesPlayer : ModPlayer
+{
+	public int Strength { get; private set; }
+	public int Dexterity { get; private set; }
+	public int Intelligence { get; private set; }
+
+	public override void ResetEffects()
+	{
+		Player.statLifeMax += (int)(Strength / 2f);
+	}
+}
