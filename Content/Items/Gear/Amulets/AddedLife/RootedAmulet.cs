b361d5d83e9be73d2deba30ef582240aabaecf08
GabeHasWon <gabriel@guevarameyer.com>
2025-01-13 21:45:32 -0600

    make rooted amulet unique

--- Content/Items/Gear/Amulets/AddedLife/RootedAmulet.cs
+++ Content/Items/Gear/Amulets/AddedLife/RootedAmulet.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections.Generic;
 using PathOfTerraria.Common.Systems.Affixes;
 using PathOfTerraria.Common.Systems.Affixes.ItemTypes;
+using PathOfTerraria.Core.Items;
 
 namespace PathOfTerraria.Content.Items.Gear.Amulets.AddedLife;
 
 public class RootedAmulet : VigorAmulet
 {
+	public override void SetStaticDefaults()
+	{
+		PoTStaticItemData def = this.GetStaticData();
+		def.IsUnique = true;
+		def.DropChance = null;
+	}
+
 	public override List<ItemAffix> GenerateAffixes()
 	{
 		var strengthAffix = (ItemAffix)Affix.CreateAffix<StrengthItemAffix>(15f, 25f);
