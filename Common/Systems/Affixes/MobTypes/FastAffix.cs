0ed89e6aa76ababf2b5be554258cd1d66dfedb70
GabeHasWon <gabriel@guevarameyer.com>
2025-01-15 10:15:04 -0600

    reorganize

---
+++ Common/Systems/Affixes/MobTypes/FastAffix.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PathOfTerraria.Common.Systems.Affixes.MobTypes;
+
+internal class FastAffix : MobAffix
+{
+	public override bool PreAI(NPC npc)
+	{
+		npc.GetGlobalNPC<SpeedUpNPC>().ExtraAISpeed += 0.3f;
+		return true;
+	}
+}
