3a7c0e3360f57c621877991eab89270aa8a6d189
GabeHasWon <gabriel@guevarameyer.com>
2025-01-21 13:56:06 -0600

    oops this is a better fix - hide droprate parameter instead of class

--- Content/Items/Pickups/PotionPickupDropper.cs
+++ Content/Items/Pickups/PotionPickupDropper.cs
@@ -48,21 +48,12 @@ internal class PotionPickupDropper : GlobalNPC
 	public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
 	{
 		LeadingConditionRule notBoss = new(new Conditions.LegacyHack_IsABoss());
-		notBoss.OnSuccess(new HiddenDrop(ModContent.ItemType<HealingPotionPickup>(), 1, 1, 1));
-		notBoss.OnSuccess(new HiddenDrop(ModContent.ItemType<ManaPotionPickup>(), 1, 1, 1));
+		notBoss.OnSuccess(ItemDropRule.Common(ModContent.ItemType<HealingPotionPickup>(), 1, 1, 1), true);
+		notBoss.OnSuccess(ItemDropRule.Common(ModContent.ItemType<ManaPotionPickup>(), 1, 1, 1), true);
 
-		notBoss.OnFailedConditions(new HiddenDrop(ModContent.ItemType<HealingPotionPickup>(), 7, 1, 1));
-		notBoss.OnFailedConditions(new HiddenDrop(ModContent.ItemType<ManaPotionPickup>(), 7, 1, 1));
+		notBoss.OnFailedConditions(ItemDropRule.Common(ModContent.ItemType<HealingPotionPickup>(), 7, 1, 1), true);
+		notBoss.OnFailedConditions(ItemDropRule.Common(ModContent.ItemType<ManaPotionPickup>(), 7, 1, 1), true);
 		
 		npcLoot.Add(notBoss);
-	}
-
-	private class HiddenDrop(int itemId, int chanceDenominator, int amountDroppedMinimum = 1, int amountDroppedMaximum = 1, int chanceNumerator = 1) 
-		: CommonDrop(itemId, chanceDenominator, amountDroppedMinimum, amountDroppedMaximum, chanceNumerator)
-	{
-		// Do nothing in here so it doesn't show on the bestiary
-		public override void ReportDroprates(List<DropRateInfo> drops, DropRateInfoChainFeed ratesInfo)
-		{
-		}
 	}
 }
