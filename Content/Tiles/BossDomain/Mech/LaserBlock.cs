422bc021087e06cb7e56eafd4be0719900bdc7de
GabeHasWon <gabriel@guevarameyer.com>
2025-03-10 14:19:03 -0500

    laser block functionality

---
+++ Content/Tiles/BossDomain/Mech/LaserBlock.cs
@@ -0,0 +1,25 @@
+﻿using Terraria.ID;
+
+namespace PathOfTerraria.Content.Tiles.BossDomain.Mech;
+
+internal class LaserBlock : ModTile
+{
+	public override void SetStaticDefaults()
+	{
+		Main.tileBrick[Type] = true;
+		Main.tileSolid[Type] = true;
+		Main.tileBlockLight[Type] = false;
+		Main.tileLighted[Type] = true;
+
+		TileID.Sets.DrawsWalls[Type] = true;
+
+		AddMapEntry(Color.Red);
+
+		DustType = DustID.Firework_Red;
+	}
+
+	public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+	{
+		(r, g, b) = (0.5f, 0.04f, 0.06f);
+	}
+}
