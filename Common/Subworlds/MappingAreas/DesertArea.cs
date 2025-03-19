d798a8f91764f26f163927c0cb28a871d908e69e
GabeHasWon <gabriel@guevarameyer.com>
2025-03-19 18:12:28 -0500

    start noise generation

--- Common/Subworlds/MappingAreas/DesertArea.cs
+++ Common/Subworlds/MappingAreas/DesertArea.cs
@@ -48,15 +48,15 @@ internal class DesertArea : MappingWorld
 			}
 		}
 
-		if (!hasPortal && ModContent.GetInstance<GrovetenderSystem>().GrovetenderWhoAmI == -1 && !NPC.AnyNPCs(ModContent.NPCType<Grovetender>()))
-		{
-			int npc = NPC.NewNPC(new EntitySource_SpawnNPC(), BossSpawnLocation.X, BossSpawnLocation.Y, ModContent.NPCType<Grovetender>());
+		//if (!hasPortal && ModContent.GetInstance<GrovetenderSystem>().GrovetenderWhoAmI == -1 && !NPC.AnyNPCs(ModContent.NPCType<Grovetender>()))
+		//{
+		//	int npc = NPC.NewNPC(new EntitySource_SpawnNPC(), BossSpawnLocation.X, BossSpawnLocation.Y, ModContent.NPCType<Grovetender>());
 
-			if (Main.netMode == NetmodeID.Server)
-			{
-				NetMessage.SendData(MessageID.SyncNPC, -1, -1, null, npc);
-			}
-		}
+		//	if (Main.netMode == NetmodeID.Server)
+		//	{
+		//		NetMessage.SendData(MessageID.SyncNPC, -1, -1, null, npc);
+		//	}
+		//}
 	}
 
 	private void GenerateTerrain(GenerationProgress progress, GameConfiguration configuration)
@@ -70,11 +70,15 @@ internal class DesertArea : MappingWorld
 		Main.spawnTileX = LeftSpawn ? 70 : Main.maxTilesX - 70;
 
 		FastNoiseLite noise = new(WorldGen._genRandSeed);
-		noise.SetFrequency(0.2f);
+		noise.SetFrequency(0.02f);
 		noise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2S);
 
 		HashSet<Point16> leafBlobs = [];
 		HashSet<int> trees = [];
+		HashSet<int> duneXs = [];
+		float cutOffY = 220;
+
+		PopulateDuneLocations(duneXs);
 
 		for (int i = 0; i < Main.maxTilesX; ++i)
 		{
@@ -84,11 +88,35 @@ internal class DesertArea : MappingWorld
 			{
 				Tile tile = Main.tile[i, j];
 				
-				if (j > 250)
+				if (j > cutOffY)
 				{
 					tile.TileType = TileID.Sand;
 					tile.HasTile = true;
 				}
+			}
+
+			if (duneXs.Contains(i))
+			{
+				cutOffY += noise.GetNoise(i, 0) * 1.2f;
+			}
+		}
+	}
+
+	private void PopulateDuneLocations(HashSet<int> duneXs)
+	{
+		List<Range> ranges = [];
+
+		for (int i = 0; i < Width / 1200 * 7; ++i)
+		{
+			int baseX = WorldGen.genRand.Next(40, Main.maxTilesX - 500);
+			ranges.Add(baseX..(baseX + WorldGen.genRand.Next(400)));
+		}
+
+		foreach (Range item in ranges)
+		{
+			for (int i = item.Start.Value; i < item.End.Value; ++i)
+			{
+				duneXs.Add(i);
 			}
 		}
 	}
