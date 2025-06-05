fa506c1c2edaa0f2a857b60ebc017409e0e9e09e
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-06-05 14:23:18 -0300

    Finish requirements + proper logging

--- Content/RoomTypes/Tavern.cs
+++ Content/RoomTypes/Tavern.cs
@@ -1,6 +1,12 @@
 ﻿using HousingAPI.Common;
 using HousingAPI.Common.Helpers;
+using Humanizer;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria.DataStructures;
 using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ObjectData;
 
 namespace PathOfTerraria.Content.RoomTypes;
 
@@ -14,7 +20,7 @@ public class Tavern : ModRoomType
 
 		if (value && results.NumTiles < 100)
 		{
-			ErrorLog += "(DEBUG) room size";
+			ErrorLog = Language.GetTextValue("TownNPCHousingFailureReasons.RoomIsTooSmall");
 			value = false;
 		}
 
@@ -23,45 +29,72 @@ public class Tavern : ModRoomType
 
 	protected override bool RoomNeeds(RoomScanner results)
 	{
-		bool failed = false;
+		const string path = $"Mods.{PoTMod.ModName}.Rooms.";
+		List<string> objects = [];
 
 		if (CountNeed(TileID.Sets.RoomNeeds.CountsAsTable, results) < 3)
 		{
-			ErrorLog += "(DEBUG) tables";
-			failed = true;
+			objects.Add(Language.GetTextValue(path + "Tavern.Error.Tables"));
 		}
 
 		if (!results.ContainsTile(TileID.Kegs))
 		{
-			ErrorLog += "(DEBUG) keg";
-			failed = true;
+			objects.Add(Language.GetTextValue(path + "Tavern.Error.Keg"));
 		}
 
-		if (CountNeed(TileID.Sets.RoomNeeds.CountsAsChair, results) < 3)
+		CountChairTypes(results, out int chairs, out int stools);
+		if (chairs < 3)
 		{
-			ErrorLog += "(DEBUG) chairs";
-			failed = true;
+			objects.Add(Language.GetTextValue(path + "Tavern.Error.Chairs"));
+		}
+
+		if (stools < 3)
+		{
+			objects.Add(Language.GetTextValue(path + "Tavern.Error.BarStools"));
 		}
 
 		if (!results.ContainsTile(TileID.Signs))
 		{
-			ErrorLog += "(DEBUG) signs";
-			failed = true;
+			objects.Add(Language.GetTextValue(path + "Tavern.Error.Sign"));
 		}
 
 		if (!results.HasRoomNeed(TileID.Sets.RoomNeeds.CountsAsTorch))
 		{
-			ErrorLog += "(DEBUG) light source";
-			failed = true;
+			objects.Add(Language.GetTextValue("Game.HouseLightSource"));
 		}
 
 		if (!results.HasRoomNeed(TileID.Sets.RoomNeeds.CountsAsDoor))
 		{
-			ErrorLog += "(DEBUG) door";
-			failed = true;
+			objects.Add(Language.GetTextValue("Game.HouseDoor"));
 		}
 
-		return !failed;
+		if (objects.Count != 0)
+		{
+			if (objects.Count == 1)
+			{
+				ErrorLog = Language.GetTextValue(path + "Common.Missing1").FormatWith(objects[0]);
+			}
+			else if (objects.Count == 2)
+			{
+				ErrorLog = Language.GetTextValue(path + "Common.Missing2").FormatWith(objects[0], objects[1]);
+			}
+			else
+			{
+				string listText = Language.GetTextValue(path + "Common.MissingList");
+				string init = string.Empty;
+
+				for (int i = 0; i < objects.Count - 2; i++)
+				{
+					init += listText.FormatWith(objects[i]);
+				}
+
+				ErrorLog = Language.GetTextValue(path + "Common.Missing3").FormatWith(init, objects[^2], objects[^1]);
+			}
+
+			return false;
+		}
+
+		return true;
 	}
 
 	private static int CountNeed(int[] set, RoomScanner results)
@@ -73,6 +106,38 @@ public class Tavern : ModRoomType
 		}
 
 		return count;
+	}
+
+	/// <summary> Manually counts chair tiles using <see cref="RoomScanner.Scanned"/>. </summary>
+	private static void CountChairTypes(RoomScanner results, out int chairs, out int barStools)
+	{
+		const int fullFrameHeight = 40;
+
+		int chairCount = 0;
+		int stoolCount = 0;
+
+		foreach (Point16 coord in results.Scanned)
+		{
+			Tile t = Main.tile[coord.X, coord.Y];
+			if (t.TileType == TileID.Chairs && TileObjectData.IsTopLeft(t))
+			{
+				if (t.TileFrameY == fullFrameHeight * 21) //Bar stool
+				{
+					stoolCount++;
+				}
+				else
+				{
+					chairCount++;
+				}
+			}
+			else if (TileID.Sets.RoomNeeds.CountsAsChair.Contains(t.TileType) && TileObjectData.IsTopLeft(t))
+			{
+				chairCount++;
+			}
+		}
+
+		chairs = chairCount;
+		barStools = stoolCount;
 	}
 
 	protected override bool AllowNPC(int npcType)
