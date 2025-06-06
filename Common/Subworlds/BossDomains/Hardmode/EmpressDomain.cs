83141daa1d8370fc67ba1e45ed25842be633ff29
GabeHasWon <gabriel@guevarameyer.com>
2025-06-05 21:21:34 -0500

    start empress domain

---
+++ Common/Subworlds/BossDomains/Hardmode/EmpressDomain.cs
@@ -0,0 +1,71 @@
+﻿using PathOfTerraria.Common.World.Generation;
+using PathOfTerraria.Common.World.Generation.Tools;
+using PathOfTerraria.Content.Projectiles.Utility;
+using PathOfTerraria.Content.Tiles.BossDomain;
+using ReLogic.Utilities;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.WorldBuilding;
+
+namespace PathOfTerraria.Common.Subworlds.BossDomains.Hardmode;
+
+internal class EmpressDomain : BossDomainSubworld
+{
+	public override int Width => 800;
+	public override int Height => 400;
+	public override (int time, bool isDay) ForceTime => ((int)Main.nightLength / 2, false);
+
+	private static bool BossSpawned = false;
+	private static bool ExitSpawned = false;
+
+	public override List<GenPass> Tasks => [new PassLegacy("Reset", ResetStep),
+		new PassLegacy("Terrain", GenTerrain)];
+
+	private void GenTerrain(GenerationProgress progress, GameConfiguration configuration)
+	{
+		Main.spawnTileX = Width / 2;
+		Main.spawnTileY = Height / 2;
+		Main.worldSurface = Height - 5;
+		Main.rockLayer = Height - 2;
+
+		StructureTools.PlaceByOrigin($"Assets/Structures/EmpressDomain/Arena", new Point16(Width / 2, Height / 2 + 5), new Vector2(0.5f, 0));
+	}
+
+	public override void OnEnter()
+	{
+		base.OnEnter();
+
+		BossSpawned = false;
+		ExitSpawned = false;
+	}
+
+	public override void Update()
+	{
+		if (!BossSpawned && NPC.AnyNPCs(NPCID.HallowBoss))
+		{
+			BossSpawned = true;
+		}
+
+		if (BossSpawned && !NPC.AnyNPCs(NPCID.HallowBoss) && !ExitSpawned)
+		{
+			ExitSpawned = true;
+
+			HashSet<Player> players = [];
+
+			foreach (Player plr in Main.ActivePlayers)
+			{
+				if (!plr.dead)
+				{
+					players.Add(plr);
+				}
+			}
+
+			IEntitySource src = Entity.GetSource_NaturalSpawn();
+			Vector2 position = Main.rand.Next([.. players]).Center - new Vector2(0, 60);
+			Projectile.NewProjectile(src, position, Vector2.Zero, ModContent.ProjectileType<ExitPortal>(), 0, 0, Main.myPlayer);
+		}
+	}
+}
