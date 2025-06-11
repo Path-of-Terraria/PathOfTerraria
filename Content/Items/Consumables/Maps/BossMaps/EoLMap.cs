0ba0db5af9ba0e0b25b5e2dbf2c62574636ce7b5
GabeHasWon <gabriel@guevarameyer.com>
2025-06-10 21:24:14 -0500

    add eol map, dropping logic

---
+++ Content/Items/Consumables/Maps/BossMaps/EoLMap.cs
@@ -0,0 +1,36 @@
+﻿using PathOfTerraria.Common.Subworlds.BossDomains.Hardmode;
+using PathOfTerraria.Core.Items;
+using SubworldLibrary;
+using Terraria.Localization;
+
+namespace PathOfTerraria.Content.Items.Consumables.Maps.BossMaps;
+
+internal class EoLMap : Map
+{
+	public override int MaxUses => GetBossUseCount();
+	public override int WorldTier => 67;
+	public override bool CanDrop => PoTItemHelper.PickItemLevel() >= 66 && NPC.downedGolemBoss;
+
+	public override void SetStaticDefaults()
+	{
+		PoTStaticItemData staticData = this.GetStaticData();
+		staticData.DropChance = 1f;
+	}
+
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
+
+		Item.Size = new Vector2(34, 38);
+	}
+
+	protected override void OpenMapInternal()
+	{
+		SubworldSystem.Enter<EmpressDomain>();
+	}
+
+	public override string GenerateName(string defaultName)
+	{
+		return Language.GetTextValue($"Mods.{PoTMod.ModName}.Items.{Name}.DisplayName");
+	}
+}
