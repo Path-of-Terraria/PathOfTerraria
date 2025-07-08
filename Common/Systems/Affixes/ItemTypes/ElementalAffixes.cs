dde0a46812bd73e03901e5fc0c6e553afb6dcb7c
GabeHasWon <gabriel@guevarameyer.com>
2025-07-07 21:29:14 -0500

    add item affix elemental resistances

---
+++ Common/Systems/Affixes/ItemTypes/ElementalAffixes.cs
@@ -0,0 +1,40 @@
+﻿using PathOfTerraria.Common.Systems.ElementalDamage;
+
+namespace PathOfTerraria.Common.Systems.Affixes.ItemTypes;
+
+internal class ResistItemAffix : ItemAffix
+{
+	public override void ApplyTooltip(Player player, Item item, AffixTooltipsHandler handler)
+	{
+		handler.AddOrModify(GetType(), item, Value, this.GetLocalization("Description"), IsCorruptedAffix);
+	}
+}
+
+internal class FireResistItemAffix : ResistItemAffix
+{
+	public override void ApplyAffix(Player player, EntityModifier modifier, Item item)
+	{
+		player.GetModPlayer<ElementalPlayer>().FireResistance += Value * 0.01f;
+	}
+}
+
+internal class ColdResistItemAffix : ResistItemAffix
+{
+	public override void ApplyAffix(Player player, EntityModifier modifier, Item item)
+	{
+		player.GetModPlayer<ElementalPlayer>().ColdResistance += Value * 0.01f;
+	}
+}
+
+internal class LightningResistItemAffix : ResistItemAffix
+{
+	public override void ApplyAffix(Player player, EntityModifier modifier, Item item)
+	{
+		player.GetModPlayer<ElementalPlayer>().LightningResistance += Value * 0.01f;
+	}
+
+	public override void ApplyTooltip(Player player, Item item, AffixTooltipsHandler handler)
+	{
+		handler.AddOrModify(GetType(), item, Value, this.GetLocalization("Description"), IsCorruptedAffix);
+	}
+}
