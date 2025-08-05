76ed61c3f6216d8c7a00235989c289b785efd2cb
Collin Herber <rustaddicts@gmail.com>
2025-08-05 18:24:06 -0400

    fix(#1088): Fixes amulets, rings, and offhands able to go into the accessory slots

--- Common/UI/Armor/AccessorySlotGlobalItem.cs
+++ Common/UI/Armor/AccessorySlotGlobalItem.cs
@@ -8,26 +8,33 @@ public sealed class AccessorySlotGlobalItem : GlobalItem
 {
 	public override bool CanEquipAccessory(Item item, Player player, int slot, bool modded)
 	{
-		bool result = true;
-
-		// TODO: Maybe create constants for those magic numbers?
-		switch (slot)
+		bool result = slot switch
 		{
-			case 4:
-				result = item.wingSlot > 0;
-				break;
-			case 5:
-				result = item.ModItem is Amulet;
-				break;
-			case 6:
-				result = item.ModItem is Offhand;
-				break;
-			case 7:
-			case 8:
-				result = item.ModItem is Ring;
-				break;
-		}
+			(int)EquipmentSlot.LeftAccessorySlot => IsNormalAccessory(item),
+			(int)EquipmentSlot.Wings => item.wingSlot > 0,
+			(int)EquipmentSlot.Necklace => item.ModItem is Amulet,
+			(int)EquipmentSlot.Offhand => item.ModItem is Offhand,
+			(int)EquipmentSlot.Ring1 or (int)EquipmentSlot.Ring2 => item.ModItem is Ring,
+			(int)EquipmentSlot.RightAccessorySlot => IsNormalAccessory(item),
+			_ => true
+		};
 
 		return result;
+	}
+	
+	private bool IsNormalAccessory(Item item)
+	{
+		return item is not null && item.accessory && item.ModItem is not Offhand && item.ModItem is not Ring && item.ModItem is not Amulet;
+	}
+	
+	private enum EquipmentSlot
+	{
+		LeftAccessorySlot = 3,
+		Wings = 4,
+		Necklace = 5,
+		Offhand = 6,
+		Ring1 = 7,
+		Ring2 = 8,
+		RightAccessorySlot = 9
 	}
 }
