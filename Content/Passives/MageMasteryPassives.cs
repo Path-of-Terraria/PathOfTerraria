101a5cb31140ad36c45772bbbae57d0c61f1d51e
Treerover <97616781+Treerover@users.noreply.github.com>
2025-08-09 22:34:06 -0400

    First pass of mastery change testing

---
+++ Content/Passives/MageMasteryPassives.cs
@@ -0,0 +1,9 @@
+﻿using PathOfTerraria.Common.Systems.ModPlayers;
+using PathOfTerraria.Common.Systems.PassiveTreeSystem;
+
+namespace PathOfTerraria.Content.Passives;
+
+internal class TestMasteryPassive: MasteryPassive
+{
+
+}
