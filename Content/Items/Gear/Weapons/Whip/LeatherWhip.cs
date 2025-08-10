b0b9468d1d9499dbe0e6537801fc572a8b0785d7
Collin Herber <rustaddicts@gmail.com>
2025-08-10 19:48:51 -0400

    feat: drastically change progression of whips and damage

--- Content/Items/Gear/Weapons/Whip/LeatherWhip.cs
+++ Content/Items/Gear/Weapons/Whip/LeatherWhip.cs
@@ -1,4 +1,5 @@
 ﻿using PathOfTerraria.Content.Projectiles.Whip;
+using PathOfTerraria.Core.Items;
 using Terraria.ID;
 
 namespace PathOfTerraria.Content.Items.Gear.Weapons.Whip;
@@ -8,11 +9,19 @@ internal class LeatherWhip : Whip
 	public override WhipDrawData DrawData => new(new(10, 26), new(0, 36, 10, 16), new(0, 64, 10, 16), new(0, 92, 10, 16), new(0, 120, 10, 18), false);
 	public override WhipSettings WhipSettings => ContentSamples.ProjectilesByType[ProjectileID.BlandWhip].WhipSettings;
 
+	public override void SetStaticDefaults()
+	{
+		base.SetStaticDefaults();
+
+		PoTStaticItemData staticData = this.GetStaticData();
+		staticData.MinDropItemLevel = 8;
+	}
+	
 	public override void SetDefaults()
 	{
 		base.SetDefaults();
 
-		Item.DefaultToWhip(ModContent.ProjectileType<WhipBaseProjectile>(), 5, 2, 4);
+		Item.DefaultToWhip(ModContent.ProjectileType<WhipBaseProjectile>(), 12, 2, 4);
 		Item.channel = true;
 		Item.value = Item.buyPrice(0, 0, 0, 20);
 	}
