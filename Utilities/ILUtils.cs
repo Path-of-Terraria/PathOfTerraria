45510094ffa2413abcfa5040896584d3dd1eb757
Mirsario <me@mirsar.io>
2025-08-17 18:58:41 +0300

    Upgraded ItemTooltipBuilder to return colors, prices, etc., without duplicating vanilla code.

---
+++ Utilities/ILUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace PathOfTerraria.Utilities;
+
+internal static class ILUtils
+{
+	/// <summary>
+	/// When there are labels pointing to the next instruction, inserts a no-op, redirects labels to it, and steps after it.
+	/// </summary>
+	public static ILCursor HijackIncomingLabels(ILCursor cursor)
+	{
+		ILLabel[] incomingLabels = cursor.IncomingLabels.ToArray();
+
+		cursor.Emit(OpCodes.Nop);
+
+		foreach (ILLabel incomingLabel in incomingLabels)
+		{
+			incomingLabel.Target = cursor.Prev;
+		}
+
+		return cursor;
+	}
+}
