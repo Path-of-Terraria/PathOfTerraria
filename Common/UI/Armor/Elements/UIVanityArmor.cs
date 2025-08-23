76384e73c6c02f0f89404a29f60d2d84d102f071
Mirsario <me@mirsar.io>
2025-08-23 17:26:48 +0300

    fix: Fixed vanity slot index regressions.

--- Common/UI/Armor/Elements/UIVanityArmor.cs
+++ Common/UI/Armor/Elements/UIVanityArmor.cs
@@ -13,20 +13,20 @@ public sealed class UIVanityArmor : UIArmorPage
 	protected override UIHoverImageItemSlot?[] GetDefaultSlots()
 	{
 		return [
-			new(DefaultFrameTexture, WingsIconTexture, new(() => (Player.armor, 4)), $"Mods.{PoTMod.ModName}.UI.Slots.10", ItemSlot.Context.EquipAccessoryVanity),
-			new(DefaultFrameTexture, HelmetIconTexture, new(() => (Player.armor, 0)), $"Mods.{PoTMod.ModName}.UI.Slots.0", ItemSlot.Context.EquipArmorVanity),
-			new(DefaultFrameTexture, NecklaceIconTexture, new(() => (Player.armor, 5)), $"Mods.{PoTMod.ModName}.UI.Slots.5", ItemSlot.Context.EquipAccessoryVanity),
+			new(DefaultFrameTexture, WingsIconTexture, new(() => (Player.armor, 14)), $"Mods.{PoTMod.ModName}.UI.Slots.10", ItemSlot.Context.EquipAccessoryVanity),
+			new(DefaultFrameTexture, HelmetIconTexture, new(() => (Player.armor, 10)), $"Mods.{PoTMod.ModName}.UI.Slots.0", ItemSlot.Context.EquipArmorVanity),
+			new(DefaultFrameTexture, NecklaceIconTexture, new(() => (Player.armor, 15)), $"Mods.{PoTMod.ModName}.UI.Slots.5", ItemSlot.Context.EquipAccessoryVanity),
 			//
-			new(DefaultFrameTexture, WeaponIconTexture, new(() => (Player.inventory, 0)), $"Mods.{PoTMod.ModName}.UI.Slots.4", 0),
-			new(DefaultFrameTexture, ChestIconTexture, new(() => (Player.armor, 1)), $"Mods.{PoTMod.ModName}.UI.Slots.1", ItemSlot.Context.EquipArmorVanity),
-			new(DefaultFrameTexture, OffhandIconTexture, new(() => (Player.armor, 6)), $"Mods.{PoTMod.ModName}.UI.Slots.6", ItemSlot.Context.EquipAccessoryVanity),
+			null,
+			new(DefaultFrameTexture, ChestIconTexture, new(() => (Player.armor, 11)), $"Mods.{PoTMod.ModName}.UI.Slots.1", ItemSlot.Context.EquipArmorVanity),
+			new(DefaultFrameTexture, OffhandIconTexture, new(() => (Player.armor, 16)), $"Mods.{PoTMod.ModName}.UI.Slots.6", ItemSlot.Context.EquipAccessoryVanity),
 			//
-			new(DefaultFrameTexture, RingIconTexture, new(() => (Player.armor, 7)), $"Mods.{PoTMod.ModName}.UI.Slots.7", ItemSlot.Context.EquipAccessoryVanity),
-			new(DefaultFrameTexture, LegsIconTexture, new(() => (Player.armor, 2)), $"Mods.{PoTMod.ModName}.UI.Slots.2", ItemSlot.Context.EquipArmorVanity),
-			new(DefaultFrameTexture, RingIconTexture, new(() => (Player.armor, 8)), $"Mods.{PoTMod.ModName}.UI.Slots.8", ItemSlot.Context.EquipAccessoryVanity),
+			new(DefaultFrameTexture, RingIconTexture, new(() => (Player.armor, 17)), $"Mods.{PoTMod.ModName}.UI.Slots.7", ItemSlot.Context.EquipAccessoryVanity),
+			new(DefaultFrameTexture, LegsIconTexture, new(() => (Player.armor, 12)), $"Mods.{PoTMod.ModName}.UI.Slots.2", ItemSlot.Context.EquipArmorVanity),
+			new(DefaultFrameTexture, RingIconTexture, new(() => (Player.armor, 18)), $"Mods.{PoTMod.ModName}.UI.Slots.8", ItemSlot.Context.EquipAccessoryVanity),
 			//
-			new(DefaultFrameTexture, MiscellaneousIconTexture, new(() => (Player.armor, 3)), $"Mods.{PoTMod.ModName}.UI.Slots.3", ItemSlot.Context.EquipAccessoryVanity),
-			new(DefaultFrameTexture, MiscellaneousIconTexture, new(() => (Player.armor, 9)), $"Mods.{PoTMod.ModName}.UI.Slots.9", ItemSlot.Context.EquipAccessoryVanity),
+			new(DefaultFrameTexture, MiscellaneousIconTexture, new(() => (Player.armor, 13)), $"Mods.{PoTMod.ModName}.UI.Slots.3", ItemSlot.Context.EquipAccessoryVanity),
+			new(DefaultFrameTexture, MiscellaneousIconTexture, new(() => (Player.armor, 19)), $"Mods.{PoTMod.ModName}.UI.Slots.9", ItemSlot.Context.EquipAccessoryVanity),
 		];
 	}
 
