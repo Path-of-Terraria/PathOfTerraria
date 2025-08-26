70f2ef4f56bcc5c01241f6617aff2cad264bee64
GabeHasWon <gabriel@guevarameyer.com>
2025-08-25 20:35:27 -0500

    add lapis lazuli, aquamarine rings & new sprites for all mana rings

---
+++ Content/Items/Gear/Rings/AddedMana/AquamarineRing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using PathOfTerraria.Common.Systems.Affixes;
+using PathOfTerraria.Common.Systems.Affixes.ItemTypes;
+
+namespace PathOfTerraria.Content.Items.Gear.Rings.AddedMana;
+
+public class AquamarineRing : Ring
+{
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
+		Item.value = Item.buyPrice(0, 0, 0, 20);
+	}
+
+	public override List<ItemAffix> GenerateImplicits()
+	{
+		return [(ItemAffix)Affix.CreateAffix<ManaAffix>(8, 12)];
+	}
+}
