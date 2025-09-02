22dcdf67fd80cecdf5da6312d67d11fdae1960e3
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-09-02 14:36:28 -0300

    Start Flame Sage Skill + ITargetable system

---
+++ Common/NPCs/SentryNPC.cs
@@ -0,0 +1,52 @@
+﻿using PathOfTerraria.Common.Systems.Synchronization.Handlers;
+using Terraria.ID;
+
+namespace PathOfTerraria.Common.NPCs;
+
+public abstract class SentryNPC : ModNPC, ITargetable
+{
+	public int Aggro;
+
+	/// <summary> The player that this sentry belongs to, indicated by <see cref="NPC.releaseOwner"/>. </summary>
+	public Player Owner => Main.player[NPC.releaseOwner];
+
+	/// <summary> Spawns the given sentry NPC at <paramref name="position"/> and assigns the owner accordingly. </summary>
+	public static void Spawn<T>(Player owner, Vector2 position) where T : SentryNPC
+	{
+		int type = ModContent.NPCType<T>();
+		if (Main.netMode == NetmodeID.MultiplayerClient)
+		{
+			ModContent.GetInstance<SpawnNPCOnServerHandler>().Send((short)type, position); //releaseOwner must be set in this context
+		}
+		else
+		{
+			var npc = NPC.NewNPCDirect(Terraria.Entity.GetSource_NaturalSpawn(), position, type);
+			npc.releaseOwner = (short)owner.whoAmI;
+			npc.netUpdate = true;
+		}
+	}
+
+	public override void SetStaticDefaults()
+	{
+		NPCID.Sets.UsesNewTargetting[Type] = true;
+	}
+
+	public override void SetDefaults()
+	{
+		NPC.aiStyle = -1;
+		NPC.lifeMax = 100;
+		Aggro = 0;
+	}
+
+	public virtual Terraria.Utilities.NPCUtils.TargetSearchResults FindTarget()
+	{
+		Terraria.Utilities.NPCUtils.TargetSearchResults results = Terraria.Utilities.NPCUtils.SearchForTarget(NPC, Terraria.Utilities.NPCUtils.TargetSearchFlag.NPCs, npcFilter: static (npc) => npc.CanBeChasedBy());
+		return results;
+	}
+
+	public virtual bool CanBeTargetedBy(NPC npc, out TargetLoader.FocusPoint focus)
+	{
+		focus = new(NPC.Hitbox, Aggro);
+		return true;
+	}
+}
---
+++ Content/Skills/Summon/SlameSage.cs
@@ -0,0 +1,70 @@
+﻿using PathOfTerraria.Common.Enums;
+using PathOfTerraria.Common.Mechanics;
+using PathOfTerraria.Common.NPCs;
+using Terraria.ID;
+
+namespace PathOfTerraria.Content.Skills.Summon;
+
+public class FlameSage : Skill
+{
+	public override int MaxLevel => 3;
+
+	public override void LevelTo(byte level)
+	{
+		Level = level;
+		Cooldown = MaxCooldown = (15 - Level * 2) * 60;
+		ManaCost = 20 - Level * 5;
+		Duration = 0;
+		WeaponType = ItemType.None;
+	}
+
+	public override void UseSkill(Player player)
+	{
+		base.UseSkill(player);
+
+		player.FindSentryRestingSpot(0, out int x, out int y, out _);
+		SentryNPC.Spawn<FlameSentry>(player, new(x, y));
+	}
+
+	private class FlameSentry : SentryNPC
+	{
+		public const int CooldownMax = 30;
+
+		public ref float Cooldown => ref NPC.ai[0];
+		public override string Texture => "Terraria/Images/Projectile_" + ProjectileID.DD2LightningAuraT1;
+
+		public override void SetStaticDefaults()
+		{
+			base.SetStaticDefaults();
+			Main.npcFrameCount[Type] = 6;
+		}
+
+		public override void SetDefaults()
+		{
+			base.SetDefaults();
+			NPC.Size = new(20);
+			NPC.noGravity = true;
+		}
+
+		public override void AI()
+		{
+			if ((Cooldown = Math.Max(Cooldown - 1, 0)) == 0)
+			{
+				Terraria.Utilities.NPCUtils.TargetSearchResults results = FindTarget();
+				NPC target = results.NearestNPC;
+
+				if (results.FoundNPC && Collision.CanHit(NPC, target))
+				{
+					Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, NPC.DirectionTo(target.Center) * 10, ProjectileID.DD2FlameBurstTowerT1Shot, NPC.damage, 3, Owner.whoAmI);
+					Cooldown = CooldownMax;
+				}
+			}
+		}
+
+		public override void FindFrame(int frameHeight)
+		{
+			NPC.frameCounter = (NPC.frameCounter + 0.2f) % Main.npcFrameCount[Type];
+			NPC.frame.Y = frameHeight * (int)NPC.frameCounter;
+		}
+	}
+}
