f00d7b9f02e4221ca1d341c54989426cc700a5f2
GabeHasWon <gabriel@guevarameyer.com>
2025-09-10 14:34:19 -0500

    fully functional stacking poison

--- Content/Buffs/ElementalBuffs/PoisonedDebuff.cs
+++ Content/Buffs/ElementalBuffs/PoisonedDebuff.cs
@@ -1,6 +1,9 @@
 ﻿using PathOfTerraria.Common.Buffs;
+using ReLogic.Content;
 using System.Collections.Generic;
+using Terraria.GameContent;
 using Terraria.ID;
+using Terraria.UI.Chat;
 
 namespace PathOfTerraria.Content.Buffs.ElementalBuffs;
 
@@ -45,10 +48,17 @@ internal class PoisonedDebuff : ModBuff
 
 internal class PoisonNPC : GlobalNPC
 {
+	public static Asset<Texture2D> PoisonIcon = null;
+
 	public override bool InstancePerEntity => true;
 
 	internal readonly List<int> Stacks = [];
 	internal float ElapsedDoT = 0;
+
+	public override void Load()
+	{
+		PoisonIcon = ModContent.Request<Texture2D>("PathOfTerraria/Assets/Misc/VFX/PoisonIcon");
+	}
 
 	public override bool PreAI(NPC npc)
 	{
@@ -58,15 +68,40 @@ internal class PoisonNPC : GlobalNPC
 			ElapsedDoT += 4 / 60f;
 		}
 
-		if (ElapsedDoT > 60)
+		Stacks.RemoveAll(x => x <= 0);
+
+		if (ElapsedDoT > 30)
 		{
-			DoTFunctionality.ApplyDoT(npc, 60, ref ElapsedDoT, Color.GreenYellow, Color.DarkGreen);
+			DoTFunctionality.ApplyDoT(npc, 30, ref ElapsedDoT, Color.Brown, Color.Red);
 		}
-		else if (ElapsedDoT > 0 && Stacks.Count == 0)
+		else if (Stacks.Count == 0)
 		{
-			DoTFunctionality.ApplyDoT(npc, (int)ElapsedDoT, ref ElapsedDoT, Color.GreenYellow, Color.DarkGreen);
+			if (ElapsedDoT > 1)
+			{
+				DoTFunctionality.ApplyDoT(npc, (int)ElapsedDoT, ref ElapsedDoT, Color.Brown, Color.Red);
+			}
+
+			ElapsedDoT = 0;
 		}
 
 		return true;
+	}
+
+	public override Color? GetAlpha(NPC npc, Color drawColor)
+	{
+		return Stacks.Count > 0 ? Color.Lerp(drawColor, Lighting.GetColor(npc.Center.ToTileCoordinates(), Color.DarkSeaGreen), 0.75f + MathF.Sin(npc.whoAmI) * 0.25f) : null;
+	}
+
+	public override void PostDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+	{
+		if (npc.GetGlobalNPC<FreezeNPC>().Frozen || Stacks.Count <= 0)
+		{
+			return;
+		}
+
+		Vector2 position = npc.Top - screenPos - new Vector2(0, 20 - npc.gfxOffY);
+		spriteBatch.Draw(PoisonIcon.Value, position, null, drawColor, 0f, PoisonIcon.Size() / 2f, 1f, SpriteEffects.None, 0);
+		string stacks = "x" + Stacks.Count;
+		ChatManager.DrawColorCodedStringWithShadow(spriteBatch, FontAssets.ItemStack.Value, stacks, position + new Vector2(8, -2), drawColor, 0f, Vector2.Zero, new(0.8f));
 	}
 }
