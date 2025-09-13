34501e06ff7ff85d0ac7c7ebd77df20aee1e9294
Treerover <97616781+Treerover@users.noreply.github.com>
2025-09-13 15:43:58 -0400

    remove useless comment

--- Content/Passives/Ranged/SmallPassives/ChanceToConsumeAmmoPassive.cs
+++ Content/Passives/Ranged/SmallPassives/ChanceToConsumeAmmoPassive.cs
@@ -7,7 +7,6 @@ internal class ChanceToConsumeAmmoPassive : Passive
 {
 	public override void BuffPlayer(Player player)
 	{
-		// Get or create a custom ModPlayer to handle ammo consumption
 		var ammoPlayer = player.GetModPlayer<AmmoConsumptionPlayer>();
 		ammoPlayer.AmmoSaveChance += (Value / 100f) * Level;
 	}
