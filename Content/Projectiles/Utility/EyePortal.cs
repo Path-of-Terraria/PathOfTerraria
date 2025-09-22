8df22c2b1d4fd3f585756a813ec6791075f466dc
GabeHasWon <gabriel@guevarameyer.com>
2025-09-22 13:26:29 -0500

    wip right click polish, will wait for another PR before continuing

--- Content/Projectiles/Utility/EyePortal.cs
+++ Content/Projectiles/Utility/EyePortal.cs
@@ -1,11 +1,10 @@
 ﻿using PathOfTerraria.Common.Projectiles;
 using PathOfTerraria.Common.Subworlds.BossDomains.Prehardmode;
-using PathOfTerraria.Common.UI;
 using PathOfTerraria.Content.Items.Consumables.Maps;
 using SubworldLibrary;
 using Terraria.GameContent;
+using Terraria.GameInput;
 using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader.IO;
 
 namespace PathOfTerraria.Content.Projectiles.Utility;
@@ -18,6 +17,8 @@ internal class EyePortal : ModProjectile, ISaveProjectile
 
 	public override void SetStaticDefaults()
 	{
+		ProjectileID.Sets.IsInteractable[Type] = true;
+
 		ClickableProjectilePlayer.RegisterProjectile(Type, static (proj, _) =>
 		{
 			if (Main.mouseRight && Main.mouseRightRelease)
@@ -32,12 +33,6 @@ internal class EyePortal : ModProjectile, ISaveProjectile
 					proj.Kill();
 				}
 			}
-
-			Tooltip.Create(new TooltipDescription
-			{
-				Identifier = "Portal",
-				SimpleTitle = Language.GetTextValue($"Mods.{PoTMod.ModName}.Misc.Enter"),
-			});
 		});
 	}
 
@@ -109,7 +104,57 @@ internal class EyePortal : ModProjectile, ISaveProjectile
 			Main.spriteBatch.Draw(tex, position, null, color, rotation, tex.Size() / 2f, 1f - i * 0.2f, SpriteEffects.None, 0);
 		}
 
+		int highlightTextureDrawMode = TryInteracting();
+
+		if (highlightTextureDrawMode == 0)
+		{
+			// If not in range, or if smart cursor is off, we don't draw the highlight texture at all.
+			return false;
+		}
+
 		return false;
+	}
+
+	private int TryInteracting()
+	{
+		if (Main.gamePaused || Main.gameMenu)
+		{
+			return 0;
+		}
+
+		bool cursorHighlights = Main.SmartCursorIsUsed || PlayerInput.UsingGamepad;
+		Player localPlayer = Main.LocalPlayer;
+		Vector2 compareSpot = localPlayer.Center;
+
+		if (!localPlayer.IsProjectileInteractibleAndInInteractionRange(Projectile, ref compareSpot))
+		{
+			return 0;
+		}
+
+		// Due to a quirk in how projectiles drawn using behindProjectiles are implemented,
+		// we need to do some math to calculate the correct world position of the mouse instead of using Main.MouseWorld directly.
+		Matrix matrix = Matrix.Invert(Main.GameViewMatrix.ZoomMatrix);
+		Vector2 position = Main.ReverseGravitySupport(Main.MouseScreen);
+		Vector2.Transform(Main.screenPosition, matrix);
+		Vector2 realMouseWorld = Main.MouseWorld;// Vector2.Transform(position, matrix) + Main.screenPosition;
+
+		bool mouseDirectlyOver = Projectile.Hitbox.Contains(realMouseWorld.ToPoint());
+		bool interactingWithThisProjectile = mouseDirectlyOver || Main.SmartInteractProj == Projectile.whoAmI;
+
+		if (!interactingWithThisProjectile || localPlayer.lastMouseInterface)
+		{
+			// 0 == Don't draw highlight texture, 1 == Draw faded highlight
+			return cursorHighlights ? 1 : 0;
+		}
+
+		if (cursorHighlights)
+		{
+			return 2; // Draw highlight texture
+		}
+		else
+		{
+			return 0;
+		}
 	}
 
 	public void SaveData(TagCompound tag)
