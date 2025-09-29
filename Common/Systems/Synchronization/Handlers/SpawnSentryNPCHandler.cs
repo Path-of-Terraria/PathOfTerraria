b24fed10e2412fe5ff9bb7db0fe5f8a36132c7c6
GabeHasWon <gabriel@guevarameyer.com>
2025-09-29 18:53:33 -0500

    sync sentry NPCs in mp

---
+++ Common/Systems/Synchronization/Handlers/SpawnSentryNPCHandler.cs
@@ -0,0 +1,36 @@
+using PathOfTerraria.Common.NPCs;
+using System.IO;
+
+namespace PathOfTerraria.Common.Systems.Synchronization.Handlers;
+
+/// <inheritdoc cref="Networking.Message.SpawnSentryNPC"/>
+internal class SpawnSentryNPCHandler : Handler
+{
+	public override Networking.Message MessageType => Networking.Message.SpawnSentryNPC;
+
+	/// <inheritdoc cref="Networking.Message.SpawnSentryNPC"/>
+	public override void Send(params object[] parameters)
+	{
+		CastParameters(parameters, out ushort type, out byte owner, out Vector2 position, out ushort timeLeft, out ushort damage);
+
+		ModPacket packet = Networking.GetPacket(MessageType);
+		packet.Write(type);
+		packet.Write(owner);
+		packet.WriteVector2(position);
+		packet.Write(timeLeft);
+		packet.Write(damage);
+		packet.Send();
+	}
+
+	internal override void ServerRecieve(BinaryReader reader)
+	{
+		ushort type = reader.ReadUInt16();
+		byte who = reader.ReadByte();
+		Vector2 position = reader.ReadVector2();
+		ushort timeLeft = reader.ReadUInt16();
+		ushort damage = reader.ReadUInt16();
+
+		NPC npc = SentryNPC.Spawn(type, Main.player[who], position, timeLeft);
+		npc.damage = damage;
+	}
+}
