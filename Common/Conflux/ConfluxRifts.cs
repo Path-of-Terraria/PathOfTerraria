9d8ec8291a65c125b242c5d0946d660f86993998
Mirsario <me@mirsar.io>
2025-10-03 14:02:08 +0300

    feat: Conflux Rift sketch.

---
+++ Common/Conflux/ConfluxRifts.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PathOfTerraria.Common.Encounters;
+using PathOfTerraria.Content.Conflux;
+using PathOfTerraria.Utilities.Terraria;
+using PathOfTerraria.Utilities.Xna;
+using SubworldLibrary;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+#nullable enable
+
+namespace PathOfTerraria.Common.Conflux;
+
+internal sealed class ConfluxRifts : ModSystem
+{
+	private static bool checkedRifts;
+
+	public override void ClearWorld()
+	{
+		checkedRifts = false;
+	}
+
+	public override void PostUpdateWorld()
+	{
+		if (!checkedRifts)
+		{
+			SpawnRifts();
+			checkedRifts = true;
+		}
+	}
+
+	/// <summary> Attempts to spawn conflux rifts, returns the amount created. </summary>
+	public static int SpawnRifts()
+	{
+		if (SubworldSystem.Current is null) { return 0; }
+
+		if (Main.netMode == NetmodeID.MultiplayerClient) { return 0; }
+
+		const int worldEdgeOffset = 40;
+		const int freeSpaceInTiles = 10;
+		const float minDistanceFromRifts = 2048f;
+		const float minDistanceFromRiftsSqr = minDistanceFromRifts * minDistanceFromRifts;
+
+		var placement = new SpawnPlacement
+		{
+			Area = new Rectangle(worldEdgeOffset, worldEdgeOffset, Main.maxTilesX - (worldEdgeOffset * 2), Main.maxTilesY - (worldEdgeOffset * 2)),
+			CollisionSize = new(freeSpaceInTiles * TileUtils.TileSizeInPixels, freeSpaceInTiles * TileUtils.TileSizeInPixels),
+			OnGround = true,
+			MinDistanceFromEnemies = 0f,
+			MinDistanceFromPlayers = 2048f,
+			MaxSearchAttempts = 4096,
+		};
+
+		int targetRifts = 3;
+		IEntitySource? source = Entity.GetSource_None();
+		var rifts = new List<Projectile>(capacity: targetRifts);
+
+		for (int i = 0; i < targetRifts; i++)
+		{
+			if (!EnemySpawning.TryFindingSpawnPosition(in placement, out Vector2 position))
+			{
+				break;
+			}
+
+			if (rifts.Any(p => p.DistanceSQ(position) < minDistanceFromRiftsSqr))
+			{
+				continue;
+			}
+
+			position.Y += 5 * TileUtils.PixelSizeInUnits;
+
+			var kind = (ConfluxRiftKind)(i % ((int)ConfluxRiftKind.Count));
+			var rift = Projectile.NewProjectileDirect(source, position, Vector2.Zero, ModContent.ProjectileType<ConfluxRift>(), 0, 0f, ai0: (float)kind);
+
+			rifts.Add(rift);
+		}
+
+		return rifts.Count;
+	}
+
+	public static void ActivateRift(Projectile rift)
+	{
+		const int extentsW = 40;
+		const int extentsH = 20;
+		Vector2 worldOrigin = rift.Center;
+		Point16 tileOrigin = worldOrigin.ToTileCoordinates16();
+
+		var area = new Vector4Int
+		{
+			X = Math.Max(tileOrigin.X - extentsW, 0),
+			Y = Math.Max(tileOrigin.Y - extentsH, 0),
+			Z = Math.Min(tileOrigin.X + extentsW, Main.maxTilesX - 1),
+			W = Math.Min(tileOrigin.Y + extentsH, Main.maxTilesY - 1),
+		};
+		var rectangle = new Rectangle(area.X, area.Y, area.Z - area.X, area.W - area.Y);
+
+		int[] possibleTypes =
+		[
+			NPCID.Zombie,
+			NPCID.ArmedZombie,
+			NPCID.Skeleton,
+			NPCID.EaterofSouls,
+			NPCID.BigEater,
+			NPCID.DemonEye,
+		];
+
+		var waves = new EncounterWave[15];
+
+		for (int waveIndex = 0; waveIndex < waves.Length; waveIndex++)
+		{
+			var spawns = new EnemySpawn[Main.rand.Next(10, 15)];
+
+			for (int spawnIndex = 0; spawnIndex < spawns.Length; spawnIndex++)
+			{
+				int type = possibleTypes[Main.rand.Next(possibleTypes.Length)];
+				spawns[spawnIndex] = new EnemySpawn
+				{
+					NpcType = new(type),
+					SpawnPosition = null,
+					SpawnPlacement = new SpawnPlacement
+					{
+						Area = default,
+						CollisionSize = default,
+						OnGround = false,
+					},
+					Effect = EnemySpawnEffect.Teleport,
+					CooldownInTicks = spawnIndex < 5 ? 0 : (uint)Main.rand.Next(30, 45),
+				};
+			}
+
+			waves[waveIndex] = new EncounterWave
+			{
+				Spawns = spawns,
+			};
+		}
+
+		EnemyEncounters.CreateEncounter(new EncounterDescription
+		{
+			Identifier = "Rift",
+			SpawnOrigin = tileOrigin,
+			ActivationOrigin = worldOrigin,
+			ActivationRange = 2048f,
+			SpawnArea = rectangle,
+			Waves = waves,
+			Music = new(MusicID.LunarBoss),
+			SceneEffectPriority = SceneEffectPriority.BossLow,
+		});
+	}
+}
---
+++ Content/Conflux/ConfluxRift.cs
@@ -0,0 +1,135 @@
+﻿using PathOfTerraria.Common.Conflux;
+using PathOfTerraria.Common.Encounters;
+using PathOfTerraria.Common.Projectiles;
+using PathOfTerraria.Common.UI;
+using PathOfTerraria.Utilities.Xna;
+using ReLogic.Content;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader.Config;
+
+#nullable enable
+
+namespace PathOfTerraria.Content.Conflux;
+
+internal enum ConfluxRiftKind : byte
+{
+	Ice,
+	Flames,
+	Lightning,
+	Count,
+}
+
+internal sealed class ConfluxRift : ModProjectile, IRightClickableProjectile
+{
+	private static Asset<Texture2D> Highlight = null!;
+
+	public ConfluxRiftKind Kind
+	{
+		get => (ConfluxRiftKind)(byte)Projectile.ai[0];
+		set => Projectile.ai[0] = (byte)value;
+	}
+
+	public override void SetStaticDefaults()
+	{
+		ProjectileID.Sets.IsInteractable[Type] = true;
+
+		if (!Main.dedServ)
+		{
+			Highlight = ModContent.Request<Texture2D>(Texture + "_Highlight");
+		}
+	}
+
+	public override void SetDefaults()
+	{
+		Projectile.friendly = false;
+		Projectile.hostile = false;
+		Projectile.timeLeft = 2;
+		Projectile.tileCollide = false;
+		Projectile.Size = new Vector2(120, 120);
+		Projectile.Opacity = 0f;
+	}
+
+	public override bool? CanDamage()
+	{
+		return false;
+	}
+
+	public override bool PreAI()
+	{
+		Projectile.timeLeft++;
+		return base.PreAI();
+	}
+
+	public override void AI()
+	{
+		Projectile.rotation += 0.05f;
+		Projectile.Opacity = MathHelper.Lerp(Projectile.Opacity, 1f, 0.25f);
+
+		if (!Main.dedServ)
+		{
+			(int torchId, int dustId, _) = GetVisualParameters();
+
+			if (Main.rand.NextBool(10))
+			{
+				Dust.NewDust(Projectile.position + new Vector2(8), Projectile.width - 16, Projectile.height - 16, dustId);
+			}
+
+			Lighting.AddLight(Projectile.Center, torchId);
+		}
+	}
+
+	public override bool PreDraw(ref Color lightColor)
+	{
+		Texture2D tex = TextureAssets.Projectile[Type].Value;
+		Vector2 position = Projectile.Center - Main.screenPosition;
+
+		(_, _, Color colorBase) = GetVisualParameters();
+
+		for (int i = 0; i < 3; ++i)
+		{
+			float rotation = (Projectile.rotation * (i % 2 == 0 ? -1 : 1)) + (i * 1.5f);
+			Color color = colorBase * ((3 - i) * 0.33f) * Projectile.Opacity;
+
+			Main.spriteBatch.Draw(tex, position, null, color, rotation, tex.Size() / 2f, 1.25f - (i * 0.25f), SpriteEffects.None, 0);
+		}
+
+		this.DrawHighlightAndCheckRightClickInteraction(Highlight.Value, position, lightColor);
+
+		return false;
+	}
+
+	private (int torchId, int dustId, Color colorBase) GetVisualParameters()
+	{
+		return Kind switch
+		{
+			ConfluxRiftKind.Ice => (TorchID.Ice, DustID.Firework_Blue, Color.AliceBlue),
+			ConfluxRiftKind.Flames => (TorchID.Red, DustID.Firework_Red, Color.OrangeRed),
+			ConfluxRiftKind.Lightning => (TorchID.Purple, DustID.WitherLightning, Color.MediumVioletRed),
+			_ => throw new NotImplementedException(),
+		};
+	}
+
+	bool IRightClickableProjectile.RightClick(Player player, bool mouseDirectlyOver)
+	{
+		if (Main.mouseRight && Main.mouseRightRelease)
+		{
+			ConfluxRifts.ActivateRift(Projectile);
+			//Projectile.active = false;
+			return true;
+		}
+
+		if (mouseDirectlyOver)
+		{
+			Tooltip.Create(new TooltipDescription
+			{
+				Identifier = "ConfluxRift",
+				SimpleTitle = Language.GetTextValue($"Mods.{PoTMod.ModName}.Misc.Enter"),
+			});
+		}
+
+		return false;
+	}
+}
