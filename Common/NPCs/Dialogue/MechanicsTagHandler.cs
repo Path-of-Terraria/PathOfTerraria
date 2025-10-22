71b5b0a63157fb671730793f0d16a15a684b9203
GabeHasWon <gabriel@guevarameyer.com>
2025-10-22 18:30:24 -0500

    functional [mechanic:type] tag.

--- Common/NPCs/Dialogue/MechanicsTagHandler.cs
+++ Common/NPCs/Dialogue/MechanicsTagHandler.cs
@@ -1,23 +1,61 @@
 ﻿using PathOfTerraria.Common.UI;
 using ReLogic.Graphics;
-using Terraria.Chat;
+using System.Collections.Generic;
 using Terraria.GameContent;
 using Terraria.Localization;
 using Terraria.UI.Chat;
 
 namespace PathOfTerraria.Common.NPCs.Dialogue;
 
+public enum Mechanic : byte
+{
+	Error,
+	Self,
+	ElementalDamage,
+	Fire,
+	Cold,
+	Lightning,
+	Chaos,
+	Ravencrest,
+	Mapping, 
+	Blocking,
+	Affixes,
+	MapAffixes,
+	ItemAffixes,
+	EnemyAffixes,
+	AreaLevel,
+	ItemRarity,
+	GrimoirePickups,
+	Questing,
+	DamageOverTime,
+}
+
 internal class MechanicsTagHandler : ITagHandler
 {
 	internal class MechanicsTextSnippet : TextSnippet
 	{
+		public string Tag
+		{
+			get
+			{
+				if (lowercase)
+				{
+					return Language.GetTextValue("Mods.PathOfTerraria.Mechanics." + mechanic + ".Tag").ToLower();
+				}
+
+				return Language.GetTextValue("Mods.PathOfTerraria.Mechanics." + mechanic + ".Tag");
+			}
+		}
+
 		private readonly Mechanic mechanic;
 		private readonly Color baseColor;
+		private readonly bool lowercase;
 
-		public MechanicsTextSnippet(Mechanic mechanic, Color baseColor)
+		public MechanicsTextSnippet(Mechanic mechanic, Color baseColor, bool lowercase)
 		{
 			this.mechanic = mechanic;
 			this.baseColor = baseColor;
+			this.lowercase = lowercase;
 
 			CheckForHover = true;
 			Text = "";
@@ -25,70 +63,106 @@ internal class MechanicsTagHandler : ITagHandler
 
 		public override bool UniqueDraw(bool justCheckingString, out Vector2 size, SpriteBatch spriteBatch, Vector2 position = default, Color color = default, float scale = 1)
 		{
-			string tag = Language.GetTextValue("Mods.PathOfTerraria.Mechanics." + mechanic + ".Tag");
-			size = ChatManager.GetStringSize(FontAssets.MouseText.Value, tag, new Vector2(scale));
+			size = ChatManager.GetStringSize(FontAssets.MouseText.Value, Tag, new Vector2(scale));
 
 			if (justCheckingString)
 			{
 				return true;
 			}
 
+			if (color.R == 0 && color.G == 0 && color.B == 0)
+			{
+				return true;
+			}
+
 			DynamicSpriteFont font = FontAssets.MouseText.Value;
-			ChatManager.DrawColorCodedStringWithShadow(Main.spriteBatch, font, tag, position, baseColor, Color.Black * 0.67f, 0f, Vector2.Zero, new Vector2(scale));
+			spriteBatch.Draw(TextureAssets.MagicPixel.Value, new Rectangle((int)position.X - 2, (int)position.Y + (int)size.Y - 10, (int)size.X + 4, 6), Color.Black);
+			spriteBatch.Draw(TextureAssets.MagicPixel.Value, new Rectangle((int)position.X, (int)position.Y + (int)size.Y - 8, (int)size.X, 2), Color.White);
+			ChatManager.DrawColorCodedStringWithShadow(spriteBatch, font, Tag, position, baseColor, Color.Black, 0f, Vector2.Zero, new Vector2(scale), spread: 2f);
 			return true;
+		}
+
+		public override Color GetVisibleColor()
+		{
+			return Color.White;
 		}
 
 		public override float GetStringLength(DynamicSpriteFont font)
 		{
-			string tag = Language.GetTextValue("Mods.PathOfTerraria.Mechanics." + mechanic + ".Tag");
-			Vector2 size = ChatManager.GetStringSize(FontAssets.MouseText.Value, tag, new Vector2(1f));
+			Vector2 size = ChatManager.GetStringSize(FontAssets.MouseText.Value, Tag, new Vector2(1f));
 			return size.X;
 		}
 
 		public override void OnHover()
 		{
+			string sub = Language.GetTextValue("Mods.PathOfTerraria.Mechanics." + mechanic + ".Description");
+			bool hasMechs = MechanicsTagInformation.AssociatedMechanics.ContainsKey(mechanic);
+
+			if (hasMechs)
+			{
+				if (Main.keyState.PressingShift())
+				{
+					sub += "\n\n" + Language.GetTextValue("Mods.PathOfTerraria.Mechanics.RelatedMechs");
+
+					foreach (Mechanic mech in MechanicsTagInformation.AssociatedMechanics[mechanic])
+					{
+						string desc = Language.GetTextValue("Mods.PathOfTerraria.Mechanics." + mech + ".Description");
+						sub += $"\n[c/888888:{Language.GetTextValue("Mods.PathOfTerraria.Mechanics." + mech + ".Tag")}:]\n{desc}\n";
+					}
+				}
+				else
+				{
+					sub += $"\n[c/888888:{Language.GetTextValue($"Mods.{PoTMod.ModName}.Mechanics.Shift")}]";
+				}
+			}
+
 			Tooltip.Create(new TooltipDescription
 			{
 				Identifier = "MechanicTag",
-				SimpleSubtitle = Language.GetTextValue("Mods.PathOfTerraria.Mechanics." + mechanic + ".Description")
+				SimpleSubtitle = sub
 			});
 		}
-	}
 
-	public enum Mechanic : byte
-	{
-		Error,
-		Self,
-		ElementalDamage,
-		Fire,
-		Cold,
-		Lightning,
-		Chaos,
-		Ravencrest
+		public override string ToString()
+		{
+			return $"Mech: " + mechanic + " Lowecase: " + lowercase;
+		}
 	}
 
 	public TextSnippet Parse(string text, Color baseColor = default, string options = null)
 	{
 		Mechanic display = Mechanic.Error;
 
-		if (Enum.TryParse(typeof(Mechanic), text, out object parsed) && parsed is Mechanic mechanic)
+		if (Enum.TryParse(typeof(Mechanic), text, true, out object parsed) && parsed is Mechanic mechanic)
 		{
 			display = mechanic;
 		}
 
-		return new MechanicsTextSnippet(display, baseColor);
+		return new MechanicsTextSnippet(display, baseColor, text == display.ToString().ToLower());
 	}
 }
 
-internal class MechanicsTagLocalizationGenerator : ILoadable
+internal class MechanicsTagInformation : ILoadable
 {
+	public static Dictionary<Mechanic, HashSet<Mechanic>> AssociatedMechanics = [];
+
 	public void Load(Mod mod)
 	{
-		foreach (MechanicsTagHandler.Mechanic enu in Enum.GetValues<MechanicsTagHandler.Mechanic>())
+		foreach (Mechanic enu in Enum.GetValues<Mechanic>())
 		{
 			_ = Language.GetOrRegister("Mods.PathOfTerraria.Mechanics." + enu + ".Tag");
 			_ = Language.GetOrRegister("Mods.PathOfTerraria.Mechanics." + enu + ".Description");
 		}
+
+		AssociatedMechanics.Add(Mechanic.Fire, [Mechanic.ElementalDamage, Mechanic.DamageOverTime]);
+		AssociatedMechanics.Add(Mechanic.Cold, [Mechanic.ElementalDamage]);
+		AssociatedMechanics.Add(Mechanic.Lightning, [Mechanic.ElementalDamage]);
+		AssociatedMechanics.Add(Mechanic.Chaos, [Mechanic.DamageOverTime]);
+		AssociatedMechanics.Add(Mechanic.Affixes, [Mechanic.ItemRarity]);
+		AssociatedMechanics.Add(Mechanic.MapAffixes, [Mechanic.Affixes]);
+		AssociatedMechanics.Add(Mechanic.ItemAffixes, [Mechanic.Affixes]);
+		AssociatedMechanics.Add(Mechanic.EnemyAffixes, [Mechanic.Affixes]);
+		AssociatedMechanics.Add(Mechanic.ItemRarity, [Mechanic.Affixes]);
 	}
 
 	public void Unload()
