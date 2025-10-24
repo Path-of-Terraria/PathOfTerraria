ae9f2288fcb181f3e81e9513daf4da53907e749e
GabeHasWon <gabriel@guevarameyer.com>
2025-10-23 19:37:51 -0500

    two new life rings

---
+++ Content/Items/Gear/Rings/AddedLife/FireDiamondRing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using PathOfTerraria.Common.Systems.Affixes;
+using PathOfTerraria.Common.Systems.Affixes.ItemTypes;
+
+namespace PathOfTerraria.Content.Items.Gear.Rings.AddedLife;
+
+public class FireDiamondRing : Ring
+{
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
+		Item.value = Item.buyPrice(0, 0, 0, 25);
+	}
+
+	public override List<ItemAffix> GenerateImplicits()
+	{
+		return [(ItemAffix)Affix.CreateAffix<BaseLifeAffix>(13, 16)];
+	}
+}
