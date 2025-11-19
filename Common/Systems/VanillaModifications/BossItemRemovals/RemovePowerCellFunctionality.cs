8895a6de95225e24e06ae9b53112a9009f8d38d9
GabeHasWon <gabehw.contact@gmail.com>
2025-11-19 00:19:58 -0600

    replaced golem power cell removal with golem power cell functionality removal

---
+++ Common/Systems/VanillaModifications/BossItemRemovals/RemovePowerCellFunctionality.cs
@@ -0,0 +1,34 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using Terraria.ID;
+
+namespace PathOfTerraria.Common.Systems.VanillaModifications.BossItemRemovals;
+
+internal class RemovePowerCellFunctionality : ModSystem
+{
+	public override void Load()
+	{
+		IL_Player.TileInteractionsUse += StopNaturalGolemSpawn;
+	}
+
+	private void StopNaturalGolemSpawn(ILContext il)
+	{
+		ILCursor c = new(il);
+
+		// Match Golem's NPC ID
+		if (!c.TryGotoNext(x => x.MatchLdcI4(NPCID.Golem)))
+		{
+			return;
+		}
+
+		ILLabel label = null;
+
+		// Match & get the label so we can exit early
+		if (!c.TryGotoNext(MoveType.After, x => x.MatchBrtrue(out label)) || label is null)
+		{
+			return;
+		}
+
+		c.Emit(OpCodes.Br, label);
+	}
+}
