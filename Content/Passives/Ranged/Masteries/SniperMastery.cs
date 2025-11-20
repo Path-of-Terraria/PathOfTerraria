178c033a63b07c321d86246b817979529e401e5c
GabeHasWon <gabehw.contact@gmail.com>
2025-11-19 19:11:20 -0600

    two of three ranged general masteries

---
+++ Content/Passives/Ranged/Masteries/SniperMastery.cs
@@ -0,0 +1,17 @@
+﻿using PathOfTerraria.Common.Systems.PassiveTreeSystem;
+
+namespace PathOfTerraria.Content.Passives;
+
+internal class SniperMastery : Passive
+{
+	internal class SniperPlayer : ModPlayer
+	{
+		public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+		{
+			if (target.DistanceSQ(Player.Center) > PoTMod.NearbyDistanceSq && modifiers.DamageType.CountsAsClass(DamageClass.Ranged))
+			{
+				modifiers.FinalDamage += Player.GetModPlayer<PassiveTreePlayer>().GetCumulativeValue<SniperMastery>() / 100f;
+			}
+		}
+	}
+}
