2b74bf42e51d6caf47ea98a0e386f34161d9699a
GabeHasWon <gabehw.contact@gmail.com>
2025-11-19 18:15:54 -0600

    start ranged cluster

--- Content/Passives/Ranged/SmallPassives/IncreasedRangedDamagePassive.cs
+++ Content/Passives/Ranged/SmallPassives/IncreasedRangedDamagePassive.cs
@@ -1,11 +1,6 @@
-using PathOfTerraria.Common.Systems.ModPlayers;
 using PathOfTerraria.Common.Systems.PassiveTreeSystem;
-using PathOfTerraria.Common.Systems.TreeSystem;
-using PathOfTerraria.Core.Items;
-using Terraria.ModLoader.IO;
 
 namespace PathOfTerraria.Content.Passives;
-
 
 internal class IncreasedRangedDamagePassive : Passive
 {
