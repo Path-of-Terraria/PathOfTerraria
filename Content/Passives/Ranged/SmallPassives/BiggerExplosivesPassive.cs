4806c31d19866ce1de0c55f9eb0701fe66e03ef3
GabeHasWon <gabehw.contact@gmail.com>
2025-12-03 23:48:44 -0600

    finish simple explosives passives

---
+++ Content/Passives/Ranged/SmallPassives/BiggerExplosivesPassive.cs
@@ -0,0 +1,31 @@
+﻿using PathOfTerraria.Common.Systems.PassiveTreeSystem;
+using Terraria.ID;
+
+namespace PathOfTerraria.Content.Passives;
+
+internal class BiggerExplosivesPassive : Passive
+{
+	internal class BiggerExplosivesProjectile : ILoadable
+	{
+		public void Load(Mod mod)
+		{
+			On_Projectile.Resize += ResizeExplosive;
+		}
+
+		private void ResizeExplosive(On_Projectile.orig_Resize orig, Projectile self, int newWidth, int newHeight)
+		{
+			if (ProjectileID.Sets.Explosive[self.type] && self.TryGetOwner(out Player player) 
+				&& player.GetModPlayer<PassiveTreePlayer>().TryGetCumulativeValue<BiggerExplosivesPassive>(out float value))
+			{
+				newWidth = (int)(newWidth * (1 + value / 100f));
+				newHeight = (int)(newHeight * (1 + value / 100f));
+			}
+
+			orig(self, newWidth, newHeight);
+		}
+
+		public void Unload()
+		{
+		}
+	}
+}
---
+++ Content/Passives/Ranged/SmallPassives/ExplosiveKnockbackPassive.cs
@@ -0,0 +1,18 @@
+﻿using PathOfTerraria.Common.Systems.PassiveTreeSystem;
+using Terraria.DataStructures;
+
+namespace PathOfTerraria.Content.Passives;
+
+internal class ExplosiveKnockbackPassive : Passive
+{
+	internal class ExplosivesKnockbackProjectile : GlobalProjectile 
+	{
+		public override void OnSpawn(Projectile projectile, IEntitySource source)
+		{
+			if (source is EntitySource_ItemUse_WithAmmo ammo && ammo.Player.GetModPlayer<PassiveTreePlayer>().TryGetCumulativeValue<StrongerExplosivesPassive>(out float value))
+			{
+				projectile.knockBack *= 1 + (value / 100f);
+			}
+		}
+	}
+}
