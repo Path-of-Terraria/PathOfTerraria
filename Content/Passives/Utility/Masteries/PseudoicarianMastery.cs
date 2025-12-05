7a8f85f4f4b098f4231752472f6af9f1af4d4df2
GabeHasWon <gabehw.contact@gmail.com>
2025-12-04 18:49:54 -0600

    one utility done, start other

---
+++ Content/Passives/Utility/Masteries/PseudoicarianMastery.cs
@@ -0,0 +1,13 @@
+﻿using PathOfTerraria.Common.Systems.PassiveTreeSystem;
+using Terraria.ID;
+
+namespace PathOfTerraria.Content.Passives.Utility.Masteries;
+
+internal class PseudoicarianMastery : Passive
+{
+	public override void BuffPlayer(Player player)
+	{
+		player.jumpSpeedBoost += Value * 0.01f * Level;
+		player.AddBuff(BuffID.Featherfall, 2);
+	}
+}
