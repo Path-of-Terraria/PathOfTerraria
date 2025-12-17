c931fb25f4d9813301d58422db9fe95aa6272505
GabeHasWon <gabehw.contact@gmail.com>
2025-12-17 16:25:10 -0600

    remaining passives

---
+++ Content/Buffs/AvariceBuff.cs
@@ -0,0 +1,9 @@
+﻿namespace PathOfTerraria.Content.Buffs;
+
+internal class AvariceBuff : ModBuff
+{
+	public override void Update(Player player, ref int buffIndex)
+	{
+		player.moveSpeed += 1.2f;
+	}
+}
