8d7496950259103eb9943e085ae9aa9c0fd1fa93
GabeHasWon <gabehw.contact@gmail.com>
2026-01-20 16:25:03 -0600

    HitInfoContainer improvements

--- Common/Systems/ElementalDamage/HitInfoContainer.cs
+++ Common/Systems/ElementalDamage/HitInfoContainer.cs
@@ -1,12 +1,24 @@
-﻿namespace PathOfTerraria.Common.Systems.ElementalDamage;
+﻿using System.Diagnostics;
+
+namespace PathOfTerraria.Common.Systems.ElementalDamage;
 
 #nullable enable
 
 /// <summary>
-/// Used to streamline calls which may take either an NPC being hit or a Player being hit.
+/// Used to streamline calls which may take either an NPC being hit or a Player being hit. Is binary; that is, either a player is hurt or an NPC is hurt, not both.
 /// </summary>
-internal readonly struct HitInfoContainer(NPC.HitInfo? hit, Player.HurtInfo? hurt)
+internal readonly struct HitInfoContainer
 {
-	internal readonly NPC.HitInfo? NPCHurt = hit;
-	internal readonly Player.HurtInfo? PlayerHurt = hurt;
+	public bool IsPlayerInfo => PlayerHurt is null;
+
+	internal readonly NPC.HitInfo? NPCHurt = null;
+	internal readonly Player.HurtInfo? PlayerHurt = null;
+
+	public HitInfoContainer(NPC.HitInfo? hit, Player.HurtInfo? hurt)
+	{
+		NPCHurt = hit;
+		PlayerHurt = hurt;
+
+		Debug.Assert(NPCHurt is null || PlayerHurt is null, "Only one info may be filled, not both.");
+	}
 }
