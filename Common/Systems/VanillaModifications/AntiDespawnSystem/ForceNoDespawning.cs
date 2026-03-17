30a01f9a7a1def832210abf3cb679d631d866af9
GabeHasWon <gabehw.contact@gmail.com>
2026-03-17 18:52:42 -0500

    finished optimization, IL-detour functionality (untested)

--- Common/Systems/VanillaModifications/AntiDespawnSystem/ForceNoDespawning.cs
+++ Common/Systems/VanillaModifications/AntiDespawnSystem/ForceNoDespawning.cs
@@ -1,5 +1,7 @@
 ﻿using PathOfTerraria.Common.Subworlds;
+using PathOfTerraria.Utilities;
 using SubworldLibrary;
+using System.Reflection;
 using Terraria.ID;
 
 namespace PathOfTerraria.Common.Systems.VanillaModifications.AntiDespawnSystem;
@@ -7,14 +9,38 @@ namespace PathOfTerraria.Common.Systems.VanillaModifications.AntiDespawnSystem;
 internal class ForceNoDespawning : GlobalNPC
 {
 	public static bool AnyPlayerIsAlive = false;
+	public static int OldLife = 0;
 
 	public override void Load()
 	{
-		On_Main.Update += EarlyUnsetPlayerFlag;
-		On_NPC.UpdateNPC += StopDespawnsByForce;
+		ILUtils.EmitILDetour(typeof(Main).GetMethod("Update", BindingFlags.NonPublic | BindingFlags.Instance), SetPlayersAlive, null);
+		ILUtils.EmitILDetour(typeof(NPC).GetMethod("UpdateNPC"), CacheLife, ForceSetNPCAlive);
+		
+		//On_Main.Update += EarlyUnsetPlayerFlag;
+		//On_NPC.UpdateNPC += StopDespawnsByForce;
 	}
 
-	private void EarlyUnsetPlayerFlag(On_Main.orig_Update orig, Main self, GameTime gameTime)
+	public static void CacheLife(NPC self, int i)
+	{
+		OldLife = self.life;
+	}
+
+	public static void ForceSetNPCAlive(NPC self, int i)
+	{
+		int life = OldLife;
+
+		// Aggressively stop despawning by forcing bosses that have health and were active to continue to be active, if we're in a boss domain and any player is alive
+		// Note - excludes many multi-segment or nonstandard NPCs, which caused a crazy bug that spammed exp and item drops infinitely
+		if ((self.boss || NPCID.Sets.ShouldBeCountedAsBoss[self.type]) && SubworldSystem.Current is BossDomainSubworld && !self.active && life > 0 && AnyPlayerIsAlive
+			&& !(self.type is NPCID.EaterofWorldsHead or NPCID.EaterofWorldsTail or NPCID.MoonLordCore or NPCID.MoonLordFreeEye or NPCID.MoonLordHead or NPCID.MoonLordHand
+			or NPCID.LunarTowerVortex or NPCID.LunarTowerStardust or NPCID.LunarTowerSolar or NPCID.LunarTowerNebula))
+		{
+			self.active = true;
+			self.life = life;
+		}
+	}
+
+	public static void SetPlayersAlive(Main self, GameTime gameTime)
 	{
 		AnyPlayerIsAlive = false;
 
@@ -26,19 +52,32 @@ internal class ForceNoDespawning : GlobalNPC
 				break;
 			}
 		}
-
-		orig(self, gameTime);
 	}
+
+	//private void EarlyUnsetPlayerFlag(On_Main.orig_Update orig, Main self, GameTime gameTime)
+	//{
+	//	AnyPlayerIsAlive = false;
+
+	//	foreach (Player player in Main.ActivePlayers)
+	//	{
+	//		if (!player.dead && !player.ghost)
+	//		{
+	//			AnyPlayerIsAlive = true;
+	//			break;
+	//		}
+	//	}
+
+	//	orig(self, gameTime);
+	//}
 
 	private void StopDespawnsByForce(On_NPC.orig_UpdateNPC orig, NPC self, int i)
 	{
 		int life = self.life;
-		int target = self.target;
 
 		orig(self, i);
 
 		// Aggressively stop despawning by forcing bosses that have health and were active to continue to be active, if we're in a boss domain and any player is alive
-		// Note - excludes EoW's Head and Tail, which caused a crazy bug that spammed exp and item drops infinitely
+		// Note - excludes many multi-segment or nonstandard NPCs, which caused a crazy bug that spammed exp and item drops infinitely
 		if ((self.boss || NPCID.Sets.ShouldBeCountedAsBoss[self.type]) && SubworldSystem.Current is BossDomainSubworld && !self.active && life > 0 && AnyPlayerIsAlive
 			&& !(self.type is NPCID.EaterofWorldsHead or NPCID.EaterofWorldsTail or NPCID.MoonLordCore or NPCID.MoonLordFreeEye or NPCID.MoonLordHead or NPCID.MoonLordHand 
 			or NPCID.LunarTowerVortex or NPCID.LunarTowerStardust or NPCID.LunarTowerSolar or NPCID.LunarTowerNebula))
