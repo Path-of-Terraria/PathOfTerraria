a7260728f9ad29994d97285b47d38e2d7bea8095
Rakan Alharbi <alhrbidotcom@gmail.com>
2026-04-16 09:38:52 +0300

    Minor improvement to tween

--- Core/Tween/TweenRunner.cs
+++ Core/Tween/TweenRunner.cs
@@ -1,21 +1,30 @@
-﻿using PathOfTerraria.Core.Tween;
+﻿using PathOfTerraria.Common.Projectiles;
 using System.Collections.Generic;
-using System.Linq;
-
 
 namespace PathOfTerraria.Core.Tween;
 
 public class TweenRunner : ModSystem
 {
     public List<ITween> Tweens = [];
+    public List<int> TweensForDeletion = [];
     public override void PreUpdateEntities()
     {
         for (int i = 0; i < Tweens.Count; i++) 
         { 
+			if(!Tweens[i].Active)
+			{
+				TweensForDeletion.Add(i);
+				continue;
+			}
             Tweens[i].Update(); 
         }
 
-        Tweens.RemoveAll(t => !t.Active);
-            
+		foreach(int forDeletion in TweensForDeletion) 
+		{
+			Tweens.RemoveAt(forDeletion);
+		}
+
+		TweensForDeletion.Clear();
+
     }
 }
