15cba33755a67be3db89fd73c2c556f4d38ba721
ShionneGitzune <fokussunightey13@gmail.com>
2026-05-03 03:42:46 +0200

    Sniper Mastery Condition
    Snipermastery was firing off at all times for ranged

--- Content/Passives/Ranged/Masteries/SniperMastery.cs
+++ Content/Passives/Ranged/Masteries/SniperMastery.cs
@@ -8,6 +8,8 @@ internal class SniperMastery : Passive
 	{
 		public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
 		{
+			if (!Player.GetModPlayer<PassiveTreePlayer>().TryGetCumulativeValue<SniperMastery>(out float value))
+				return;
 			if (target.DistanceSQ(Player.Center) > PoTMod.NearbyDistanceSq && modifiers.DamageType.CountsAsClass(DamageClass.Ranged))
 			{
 				modifiers.SourceDamage *= Player.GetModPlayer<PassiveTreePlayer>().GetCumulativeValue<SniperMastery>() / 100f;
