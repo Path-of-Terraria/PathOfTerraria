a73cd198177157020e275bd7d37d896a1b518665
Collin Herber <rustaddicts@gmail.com>
2026-05-06 11:21:57 -0400

    chore: pr comments

--- Common/Systems/ModPlayers/ProjectileModifierPlayer.cs
+++ Common/Systems/ModPlayers/ProjectileModifierPlayer.cs
@@ -5,7 +5,7 @@ using Terraria.ID;
 
 namespace PathOfTerraria.Common.Systems.ModPlayers;
 
-public class ProjectileModifierProjectile : GlobalProjectile, IOnContinuouslyUpdateDamage
+public class ProjectileModifierProjectile : GlobalProjectile
 {
 	/// <summary>
 	/// Tags a projectile spawned directly by a player (such as where <c>source is EntitySource_Parent { Entity: Player }</c>) as modifiable even if
@@ -14,14 +14,6 @@ public class ProjectileModifierProjectile : GlobalProjectile, IOnContinuouslyUpd
 	internal const string ModifiableProjectileTag = "Modifiable";
 
 	private static readonly HashSet<int> InvalidProjectilesToMove = [];
-
-	private bool continuouslyUpdateDamage;
-	private float unmodifiedDamage;
-	private int lastUpdatedDamage;
-	private int owner = -1;
-	private int projectileIndex = -1;
-
-	public override bool InstancePerEntity => true;
 
 	public override void SetStaticDefaults()
 	{
@@ -75,43 +67,6 @@ public class ProjectileModifierProjectile : GlobalProjectile, IOnContinuouslyUpd
 		}
 	}
 
-	public void OnContinuouslyUpdateDamage()
-	{
-		UpdateDamage(Main.projectile.IndexInRange(projectileIndex) ? Main.projectile[projectileIndex] : null);
-	}
-
-	public override void PostAI(Projectile projectile)
-	{
-		UpdateDamage(projectile);
-	}
-
-	private void UpdateDamage(Projectile projectile)
-	{
-		if (!continuouslyUpdateDamage || projectile is null || !Main.player.IndexInRange(owner))
-		{
-			return;
-		}
-
-		Player player = Main.player[owner];
-
-		if (!player.active || !projectile.active)
-		{
-			return;
-		}
-
-		int damage = Math.Max(1, (int)player.GetTotalDamage(projectile.DamageType).ApplyTo(unmodifiedDamage));
-
-		if (damage == lastUpdatedDamage)
-		{
-			return;
-		}
-
-		projectile.damage = damage;
-		projectile.originalDamage = damage;
-		projectile.netUpdate = true;
-		lastUpdatedDamage = damage;
-	}
-
 	private void TryStartContinuousDamageUpdates(Projectile projectile, Player player, Item item)
 	{
 		if (player is null || item is null || projectile.damage <= 0 || !ShouldContinuouslyUpdateDamage(projectile, item))
@@ -120,17 +75,16 @@ public class ProjectileModifierProjectile : GlobalProjectile, IOnContinuouslyUpd
 		}
 
 		StatModifier damageModifier = player.GetTotalDamage(projectile.DamageType);
-		unmodifiedDamage = Unapply(damageModifier, projectile.damage);
-		lastUpdatedDamage = projectile.damage;
-		owner = player.whoAmI;
-		projectileIndex = projectile.whoAmI;
-		continuouslyUpdateDamage = unmodifiedDamage > 0;
+		projectile.originalDamage = Math.Max(1, (int)Unapply(damageModifier, projectile.damage));
+		projectile.ContinuouslyUpdateDamageStats = true;
 	}
 
 	private static bool ShouldContinuouslyUpdateDamage(Projectile projectile, Item item)
 	{
 		return projectile.ModProjectile is not IOnContinuouslyUpdateDamage
-			&& (projectile.aiStyle == ProjAIStyleID.Yoyo || projectile.minion || projectile.sentry || item.channel);
+			&& !projectile.minion
+			&& !projectile.sentry
+			&& (projectile.aiStyle == ProjAIStyleID.Yoyo || item.channel);
 	}
 
 	private static float Unapply(StatModifier modifier, float damage)
