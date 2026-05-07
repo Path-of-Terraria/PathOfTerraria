28a6dd75b704c8b84aa233f323b1ddb3cdab02c5
Collin Herber <rustaddicts@gmail.com>
2026-05-07 08:16:51 -0400

    fix(#2005): Reduced cooldown passive not using correct passives.json value and incorrect tooltip

--- Content/Passives/Misc/SkillPassives.cs
+++ Content/Passives/Misc/SkillPassives.cs
@@ -1,17 +1,12 @@
-﻿using Humanizer;
-using PathOfTerraria.Common.Systems.ModPlayers.SkillPlayers;
+﻿using PathOfTerraria.Common.Systems.ModPlayers.SkillPlayers;
 using PathOfTerraria.Common.Systems.PassiveTreeSystem;
 
 namespace PathOfTerraria.Content.Passives.Misc;
 
 internal class ReducedSkillCooldownPassive : Passive
 {
-	private const float CooldownBuff = 0.95f;
-
-	public override string DisplayTooltip => base.DisplayTooltip.FormatWith((1 - CooldownBuff) * 100);
-
 	public override void BuffPlayer(Player player)
 	{
-		player.GetModPlayer<SkillCombatPlayer>().GlobalBuff.Cooldown *= CooldownBuff;
+		player.GetModPlayer<SkillCombatPlayer>().GlobalBuff.Cooldown *= 1 - Value / 100f;
 	}
 }
