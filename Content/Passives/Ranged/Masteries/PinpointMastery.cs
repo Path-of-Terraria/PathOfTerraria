6ae90e2f93aa831aa0eddf33e8e6a2413a484c42
Treerover <97616781+Treerover@users.noreply.github.com>
2026-05-22 12:26:14 -0400

    feat: Adds Pinpoint Mastery to the ranged damage cluster. All projectiles gain +1 Pierce

---
+++ Content/Passives/Ranged/Masteries/PinpointMastery.cs
@@ -0,0 +1,21 @@
+using PathOfTerraria.Common.Systems.PassiveTreeSystem;
+using Terraria.DataStructures;
+
+namespace PathOfTerraria.Content.Passives;
+
+internal class PinpointMastery : Passive
+{
+	internal class PinpointProjectile : GlobalProjectile
+	{
+		public override void OnSpawn(Projectile projectile, IEntitySource source)
+		{
+			if (!projectile.friendly || projectile.penetrate < 0 || !projectile.TryGetOwner(out Player player)
+			    || !player.GetModPlayer<PassiveTreePlayer>().TryGetCumulativeValue<PinpointMastery>(out float value))
+			{
+				return;
+			}
+
+			projectile.penetrate += (int)value;
+		}
+	}
+}
