b8deadb5476db08f2528f8b220df6e2e5141a543
Treerover <97616781+Treerover@users.noreply.github.com>
2026-05-23 23:16:43 -0400

    feat: Adds immolations stride mastery "Gain 30% increased movement speed and Fire damage while you have the Ignited or Scorched debuff."

--- Content/Passives/Melee/Masteries/ImmolationStrideMastery.cs
+++ Content/Passives/Melee/Masteries/ImmolationStrideMastery.cs
@@ -1,5 +1,6 @@
 using PathOfTerraria.Common.Systems.ElementalDamage;
 using PathOfTerraria.Common.Systems.PassiveTreeSystem;
+using PathOfTerraria.Content.Buffs;
 using PathOfTerraria.Content.Buffs.ElementalBuffs;
 
 namespace PathOfTerraria.Content.Passives.Melee.Masteries;
@@ -8,10 +9,11 @@ internal class ImmolationStrideMastery : Passive
 {
 	public override void BuffPlayer(Player player)
 	{
-		if (player.HasBuff<IgnitedDebuff>())
+		if (player.HasBuff<IgnitedDebuff>() || player.HasBuff<ScorchingFlamesDebuff>())
 		{
-			player.moveSpeed += 1;
-			player.GetModPlayer<ElementalPlayer>().Container[ElementType.Fire].Multiplier *= 1.3f;
+			float value = Value / 100f;
+			player.moveSpeed += value;
+			player.GetModPlayer<ElementalPlayer>().Container[ElementType.Fire].Multiplier *= (1 + value);
 		}
 	}
 }
