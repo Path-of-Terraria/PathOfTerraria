91a54c3d1dc7e92c2ceb1b055dc87a0b6e0ae168
GabeHasWon <gabehw.contact@gmail.com>
2026-05-25 14:29:30 -0500

    feat: Add player class and level to multiplayer team nametags, in the format "Name (Class - level)"

---
+++ Common/Systems/ModPlayers/RemoteInfoPlayer.cs
@@ -0,0 +1,79 @@
+﻿using PathOfTerraria.Common.Classing;
+using PathOfTerraria.Common.Systems.Synchronization;
+using System.IO;
+
+namespace PathOfTerraria.Common.Systems.ModPlayers;
+
+/// <summary>
+/// Handles requesting certain information that is visual-only.
+/// </summary>
+internal class RemoteInfoPlayer : ModPlayer
+{
+	/// <summary>
+	/// Sends all "visual info" to other clients. This info isn't usually used for multiplayer gameplay otherwise.
+	/// </summary>
+	public class SendRemoteInfoHandler : Handler
+	{
+		public static void Send(short returnToPlayer = -1)
+		{
+			Player plr = Main.LocalPlayer;
+
+			ModPacket packet = Networking.GetPacket<SendRemoteInfoHandler>();
+			packet.Write((byte)plr.GetModPlayer<ClassingPlayer>().Class);
+			packet.Write((byte)plr.GetModPlayer<ExpModPlayer>().Level);
+			packet.Write(returnToPlayer);
+			packet.Send();
+		}
+
+		internal override void Receive(BinaryReader reader, byte sender)
+		{
+			int who = Main.dedServ ? sender : reader.ReadByte();
+			Player plr = Main.player[who];
+			plr.GetModPlayer<ClassingPlayer>().Class = (StarterClass)reader.ReadByte();
+			plr.GetModPlayer<ExpModPlayer>().Level = reader.ReadByte();
+
+			if (Main.dedServ)
+			{
+				short returnToPlayer = reader.ReadInt16();
+
+				ModPacket packet = Networking.GetPacket<SendRemoteInfoHandler>();
+				packet.Write((byte)plr.whoAmI);
+				packet.Write((byte)plr.GetModPlayer<ClassingPlayer>().Class);
+				packet.Write((byte)plr.GetModPlayer<ExpModPlayer>().Level);
+				packet.Send(returnToPlayer);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Requests a <see cref="SendRemoteInfoHandler"/> from every other joined player.
+	/// </summary>
+	public class RequestRemoteInfoHandler : Handler
+	{
+		public static void Send()
+		{
+			ModPacket packet = Networking.GetPacket<RequestRemoteInfoHandler>();
+			packet.Send();
+		}
+
+		internal override void Receive(BinaryReader reader, byte sender)
+		{
+			if (Main.dedServ)
+			{
+				ModPacket packet = Networking.GetPacket<RequestRemoteInfoHandler>();
+				packet.Write(sender);
+				packet.Send();
+			}
+			else
+			{
+				SendRemoteInfoHandler.Send(reader.ReadByte());
+			}
+		}
+	}
+
+	public override void OnEnterWorld()
+	{
+		RequestRemoteInfoHandler.Send();
+		SendRemoteInfoHandler.Send();
+	}
+}
